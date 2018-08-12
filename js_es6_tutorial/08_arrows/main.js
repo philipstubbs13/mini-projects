f92f4b4ddd99f087e1e42c8d84c202734d030ace
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-12 18:00:54 -0500

    arrow functions

---
+++ js_es6_tutorial/08_arrows/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+// function Prefixer(prefix){
+//     this.prefix = prefix;
+// }
+
+// Prefixer.prototype.prefixArray = function(arr){
+//     let that = this;
+//     return arr.map(function(x){
+//         console.log(th.prefix + x);
+//     });
+// }
+
+// let pre = new Prefixer('Hello ');
+// pre.prefixArray(['Brad', 'Jeff']);
+
+
+// function Prefixer(prefix){
+//     this.prefix = prefix;
+// }
+
+// Prefixer.prototype.prefixArray = function(arr){
+//     return arr.map((x) => {
+//         console.log(this.prefix + x);
+//     });
+// }
+
+// let pre = new Prefixer('Hello ');
+// pre.prefixArray(['Brad', 'Jeff']);
+
+// let add = function(a,b){
+//     let sum = a+b;
+//     console.log(sum);
+//     return false;
+// }
+
+let add = (a,b) => {
+    let sum = a+b;
+    console.log(sum);
+    return false;
+}
+
+add(10,2);
