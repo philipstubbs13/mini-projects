983c89dd8a4c4acf9488dd6c8552a5f7b0304673
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-12 13:22:12 -0500

    added basic html and css

--- fun-food-friends/src/App.js
+++ fun-food-friends/src/App.js
@@ -5,17 +5,30 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>Fun Food Friends</h1>
+              
+            </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container'>
+          <section className='add-item'>
+              <form>
+                <input type="text" name="username" placeholder="What's your name?" />
+                <input type="text" name="currentItem" placeholder="What are you bringing?" />
+                <button>Add Item</button>
+              </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
