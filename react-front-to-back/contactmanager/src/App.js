fc019d3abb73a044a2a07b6c4acb68b1ebfa3202
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-26 22:32:37 -0500

    added bootstrap styling

--- react-front-to-back/contactmanager/src/App.js
+++ react-front-to-back/contactmanager/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,10 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Header branding="Contact Manager" />
-				<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-				<Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333" />
+				<div className="container">
+					<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+					<Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333" />
+				</div>
 			</div>
 		);
 	}
