0406866b905da3a341e7aa01cd4e560488217cb0
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-27 18:58:10 -0500

    events in react

--- react-front-to-back/contactmanager/src/components/Contact.js
+++ react-front-to-back/contactmanager/src/components/Contact.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+	state = {};
+
+	onShowClick = () => {};
 	render() {
 		const { name, email, phone } = this.props.contact;
 		return (
 			<div className="card card-body mb-3">
-				<h4>{name}</h4>
+				<h4>
+					{name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+				</h4>
 				<ul className="list-group">
 					<li className="list-group-item">Email: {email}</li>
 					<li className="list-group-item">Phone: {phone}</li>
