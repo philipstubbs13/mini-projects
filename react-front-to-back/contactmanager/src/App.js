de7b848847020ea7ae7de4be70b0619efa2674c3
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-26 22:00:20 -0500

    components and props

--- react-front-to-back/contactmanager/src/App.js
+++ react-front-to-back/contactmanager/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>The App Component</h1>
+				<Header branding="Contact Manager" />
+				<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+				<Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333" />
 			</div>
 		);
 	}
