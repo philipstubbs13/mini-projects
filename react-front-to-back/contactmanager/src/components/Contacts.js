89f45360e9b2677df3a974e4295654e35ce13481
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-27 18:47:46 -0500

    creating state

---
+++ react-front-to-back/contactmanager/src/components/Contacts.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Contact from './Contact';
+
+class Contacts extends Component {
+	state = {
+		contacts: [
+			{
+				id: 1,
+				name: 'John Doe',
+				email: 'jdoe@gmail.com',
+				phone: '555-555-5555',
+			},
+			{
+				id: 2,
+				name: 'Karen Williams',
+				email: 'karen@gmail.com',
+				phone: '222-222-2222',
+			},
+			{
+				id: 3,
+				name: 'Henry Johnson',
+				email: 'henry@gmail.com',
+				phone: '111-111-1111',
+			},
+		],
+	};
+	render() {
+		const { contacts } = this.state;
+		return (
+			<div>
+				{contacts.map(contact => (
+					<Contact key={contact.id} contact={contact} />
+				))}
+			</div>
+		);
+	}
+}
+
+export default Contacts;
