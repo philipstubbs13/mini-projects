369c8ed2b281eef2febd5b3f6fb7752e35493587
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-27 19:14:52 -0500

    changing state

--- react-front-to-back/contactmanager/src/components/Contact.js
+++ react-front-to-back/contactmanager/src/components/Contact.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
-	state = {};
+	state = {
+		showContactInfo: true,
+	};
 
-	onShowClick = () => {};
 	render() {
 		const { name, email, phone } = this.props.contact;
+		const { showContactInfo } = this.state;
+
 		return (
 			<div className="card card-body mb-3">
 				<h4>
-					{name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+					{name}{' '}
+					<i
+						onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
+						className="fas fa-sort-down"
+					/>
 				</h4>
-				<ul className="list-group">
-					<li className="list-group-item">Email: {email}</li>
-					<li className="list-group-item">Phone: {phone}</li>
-				</ul>
+				{showContactInfo ? (
+					<ul className="list-group">
+						<li className="list-group-item">Email: {email}</li>
+						<li className="list-group-item">Phone: {phone}</li>
+					</ul>
+				) : null}
 			</div>
 		);
 	}
