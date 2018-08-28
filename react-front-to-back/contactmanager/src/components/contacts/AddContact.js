25158c8f96dcd5418cdedf1839f32c5cb63787be
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-28 06:14:34 -0500

    error checking and display

--- react-front-to-back/contactmanager/src/components/contacts/AddContact.js
+++ react-front-to-back/contactmanager/src/components/contacts/AddContact.js
@@ -8,12 +8,29 @@ class AddContact extends Component {
 		name: '',
 		email: '',
 		phone: '',
+		errors: [],
 	};
 
 	onSubmit = (dispatch, e) => {
 		e.preventDefault();
 
 		const { name, email, phone } = this.state;
+
+		// Check For Errors
+		if (name === '') {
+			this.setState({ errors: { name: 'Name is required ' } });
+			return;
+		}
+
+		if (email === '') {
+			this.setState({ errors: { email: 'Email is required ' } });
+			return;
+		}
+
+		if (phone === '') {
+			this.setState({ errors: { phone: 'Phone is required ' } });
+			return;
+		}
 
 		const newContact = {
 			id: uuid(),
@@ -29,13 +46,14 @@ class AddContact extends Component {
 			name: '',
 			email: '',
 			phone: '',
+			errors: [],
 		});
 	};
 
 	onChange = e => this.setState({ [e.target.name]: e.target.value });
 
 	render() {
-		const { name, email, phone } = this.state;
+		const { name, email, phone, errors } = this.state;
 
 		return (
 			<Consumer>
@@ -52,6 +70,7 @@ class AddContact extends Component {
 										placeholder="Enter Name"
 										value={name}
 										onChange={this.onChange}
+										error={errors.name}
 									/>
 									<TextInputGroup
 										label="Email"
@@ -60,6 +79,7 @@ class AddContact extends Component {
 										placeholder="Enter Email"
 										value={email}
 										onChange={this.onChange}
+										error={errors.email}
 									/>
 									<TextInputGroup
 										label="Phone"
@@ -67,6 +87,7 @@ class AddContact extends Component {
 										placeholder="Enter Phone"
 										value={phone}
 										onChange={this.onChange}
+										error={errors.phone}
 									/>
 									<input type="submit" value="Add Contact" className="btn btn-light btn-block" />
 								</form>
