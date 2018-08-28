f10b7bb51d9fed1bcb9808a5c41fb34acfe74784
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-28 05:08:42 -0500

    controlled components and onchange

--- react-front-to-back/contactmanager/src/components/contacts/AddContact.js
+++ react-front-to-back/contactmanager/src/components/contacts/AddContact.js
@@ -7,13 +7,20 @@ class AddContact extends Component {
 		phone: '',
 	};
 
+	onSubmit = e => {
+		e.preventDefault();
+		console.log(this.state);
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+
 	render() {
 		const { name, email, phone } = this.state;
 		return (
 			<div className="card mb-3">
 				<div className="card-header">Add Contact</div>
 				<div className="card-body">
-					<form>
+					<form onSubmit={this.onSubmit}>
 						<div className="form-group">
 							<label htmlFor="name">Name</label>
 							<input
@@ -22,6 +29,7 @@ class AddContact extends Component {
 								className="form-control form-control-lg"
 								placeholder="Enter Name..."
 								value={name}
+								onChange={this.onChange}
 							/>
 						</div>
 						<div className="form-group">
@@ -32,6 +40,7 @@ class AddContact extends Component {
 								className="form-control form-control-lg"
 								placeholder="Enter Email..."
 								value={email}
+								onChange={this.onChange}
 							/>
 						</div>
 						<div className="form-group">
@@ -42,6 +51,7 @@ class AddContact extends Component {
 								className="form-control form-control-lg"
 								placeholder="Enter Phone..."
 								value={phone}
+								onChange={this.onChange}
 							/>
 						</div>
 						<input type="submit" value="Add Contact" className="btn btn-light btn-block" />
