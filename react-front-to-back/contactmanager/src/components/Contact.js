d4220986eaac6a25e715d713efd5084bd1177b77
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-27 19:30:25 -0500

    changing state from another component

--- react-front-to-back/contactmanager/src/components/Contact.js
+++ react-front-to-back/contactmanager/src/components/Contact.js
@@ -6,6 +6,10 @@ class Contact extends Component {
 		showContactInfo: true,
 	};
 
+	onDeleteClick = () => {
+		this.props.deleteClickHandler();
+	};
+
 	render() {
 		const { name, email, phone } = this.props.contact;
 		const { showContactInfo } = this.state;
@@ -17,6 +21,12 @@ class Contact extends Component {
 					<i
 						onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
 						className="fas fa-sort-down"
+						style={{ cursor: 'pointer' }}
+					/>
+					<i
+						className="fas fa-times"
+						style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+						onClick={this.onDeleteClick}
 					/>
 				</h4>
 				{showContactInfo ? (
@@ -32,6 +42,7 @@ class Contact extends Component {
 
 Contact.propTypes = {
 	contact: PropTypes.object.isRequired,
+	deleteClickHandler: PropTypes.func.isRequired,
 };
 
 export default Contact;
