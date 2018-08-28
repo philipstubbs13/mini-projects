2e6021ae10566f06774f0d58885ea4179039ca22
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-27 21:14:11 -0500

    context api and provider state

--- react-front-to-back/contactmanager/src/components/Contacts.js
+++ react-front-to-back/contactmanager/src/components/Contacts.js
@@ -1,30 +1,8 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class Contacts extends Component {
-	state = {
-		contacts: [
-			{
-				id: 1,
-				name: 'John Doe',
-				email: 'jdoe@gmail.com',
-				phone: '555-555-5555',
-			},
-			{
-				id: 2,
-				name: 'Karen Williams',
-				email: 'karen@gmail.com',
-				phone: '222-222-2222',
-			},
-			{
-				id: 3,
-				name: 'Henry Johnson',
-				email: 'henry@gmail.com',
-				phone: '111-111-1111',
-			},
-		],
-	};
-
 	deleteContact = id => {
 		const { contacts } = this.state;
 
@@ -36,17 +14,23 @@ class Contacts extends Component {
 	};
 
 	render() {
-		const { contacts } = this.state;
 		return (
-			<React.Fragment>
-				{contacts.map(contact => (
-					<Contact
-						key={contact.id}
-						contact={contact}
-						deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-					/>
-				))}
-			</React.Fragment>
+			<Consumer>
+				{value => {
+					const { contacts } = value;
+					return (
+						<React.Fragment>
+							{contacts.map(contact => (
+								<Contact
+									key={contact.id}
+									contact={contact}
+									deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+								/>
+							))}
+						</React.Fragment>
+					);
+				}}
+			</Consumer>
 		);
 	}
 }
