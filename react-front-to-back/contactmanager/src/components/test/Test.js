3cb52866f39cf26641a6ce19eca1ff5fe2987a93
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-30 07:39:57 -0500

    get requests

--- react-front-to-back/contactmanager/src/components/test/Test.js
+++ react-front-to-back/contactmanager/src/components/test/Test.js
@@ -2,43 +2,53 @@ import React, { Component } from 'react';
 
 class Test extends Component {
 	state = {
-		test: 'test',
+		title: '',
+		body: '',
 	};
 
 	componentDidMount() {
-		console.log('componentDidMount...');
+		fetch('https://jsonplaceholder.typicode.com/posts/1')
+			.then(response => response.json())
+			.then(data =>
+				this.setState({
+					title: data.title,
+					body: data.body,
+				})
+			);
 	}
 
-	componentWillMount() {
-		console.log('componentWillMount...');
-	}
+	// componentWillMount() {
+	// 	console.log('componentWillMount...');
+	// }
 
-	componentDidUpdate() {
-		console.log('componentDidUpdate...');
-	}
+	// componentDidUpdate() {
+	// 	console.log('componentDidUpdate...');
+	// }
 
-	componentWillUpdate() {
-		console.log('componentWillUpdate...');
-	}
+	// componentWillUpdate() {
+	// 	console.log('componentWillUpdate...');
+	// }
 
-	componentWillReceiveProps(nextProps, nextState) {
-		console.log('componentWillReceiveProps...');
-	}
+	// componentWillReceiveProps(nextProps, nextState) {
+	// 	console.log('componentWillReceiveProps...');
+	// }
 
-	static getDerivedStateFromProps(nextProps, prevState) {
-		return {
-			test: 'something',
-		};
-	}
+	// static getDerivedStateFromProps(nextProps, prevState) {
+	// 	return {
+	// 		test: 'something',
+	// 	};
+	// }
 
-	getSnapshotBeforeUpdate(prevProps, prevState) {
-		console.log('getSnapshotBeforeUpdate...');
-	}
+	// getSnapshotBeforeUpdate(prevProps, prevState) {
+	// 	console.log('getSnapshotBeforeUpdate...');
+	// }
 
 	render() {
+		const { title, body } = this.state;
 		return (
 			<div>
-				<h1>Test Component</h1>
+				<h1>{title}</h1>
+				<p>{body}</p>
 			</div>
 		);
 	}
--- react-front-to-back/contactmanager/src/context.js
+++ react-front-to-back/contactmanager/src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -21,28 +22,13 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
 	state = {
-		contacts: [
-			{
-				id: 1,
-				name: 'John Doe',
-				email: 'jdoe@gmail.com',
-				phone: '555-555-5555',
-			},
-			{
-				id: 2,
-				name: 'Karen Williams',
-				email: 'karen@gmail.com',
-				phone: '222-222-2222',
-			},
-			{
-				id: 3,
-				name: 'Henry Johnson',
-				email: 'henry@gmail.com',
-				phone: '111-111-1111',
-			},
-		],
+		contacts: [],
 		dispatch: action => this.setState(state => reducer(state, action)),
 	};
+
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/users').then(res => this.setState({ contacts: res.data }));
+	}
 
 	render() {
 		return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>;
