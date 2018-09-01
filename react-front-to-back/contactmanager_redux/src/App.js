ad683c6a58aedd7911f56ba32340e86f5caa46fe
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-01 10:47:38 -0500

    redux store setup

--- react-front-to-back/contactmanager_redux/src/App.js
+++ react-front-to-back/contactmanager_redux/src/App.js
@@ -7,28 +7,33 @@ import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contact/add" component={AddContact} />
-              <Route exact path="/contact/edit/:id" component={EditContact} />
-              <Route exact path="/about" component={About} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Header branding="Contact Manager" />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Contacts} />
+								<Route exact path="/contact/add" component={AddContact} />
+								<Route exact path="/contact/edit/:id" component={EditContact} />
+								<Route exact path="/about" component={About} />
+								<Route component={NotFound} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
