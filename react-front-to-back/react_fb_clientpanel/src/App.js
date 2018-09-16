1d27ee01435ac0d99de7aedf9b6f518ce4f71e3b
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-16 18:41:12 -0500

    client details component

--- react-front-to-back/react_fb_clientpanel/src/App.js
+++ react-front-to-back/react_fb_clientpanel/src/App.js
@@ -7,6 +7,7 @@ import store from './store';
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import ClientDetails from './components/clients/ClientDetails';
 import './App.css';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
 							<Switch>
 								<Route exact path="/" component={Dashboard} />
 								<Route exact path="/clients/add" component={AddClient} />
+								<Route exact path="/client/:id" component={ClientDetails} />
 							</Switch>
 						</div>
 					</div>
