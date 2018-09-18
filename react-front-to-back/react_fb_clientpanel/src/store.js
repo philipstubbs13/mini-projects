0a2219a549069f138fd623afaa45106733e41d77
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-18 08:34:19 -0500

    notify state and alert component

--- react-front-to-back/react_fb_clientpanel/src/store.js
+++ react-front-to-back/react_fb_clientpanel/src/store.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
-// @todo
+import notifyReducer from './reducers/notifyReducer'
 
 const firebaseConfig = {
 	apiKey: "AIzaSyASGEFECmAW_ej_voli2VrvjuWo17yJ50Q",
@@ -38,7 +38,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
 	firebase: firebaseReducer,
-	firestore: firestoreReducer // <- needed if using firestore
+	firestore: firestoreReducer,
+	notify: notifyReducer
 })
 
 // Crete initial state
