d05b95b6b87d2eed936b5a37c1a23286af7f794d
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-18 08:47:05 -0500

    settings reducer and actions

--- react-front-to-back/react_fb_clientpanel/src/store.js
+++ react-front-to-back/react_fb_clientpanel/src/store.js
@@ -5,6 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
 import notifyReducer from './reducers/notifyReducer'
+import settingsReducer from './reducers/settingsReducer'
 
 const firebaseConfig = {
 	apiKey: "AIzaSyASGEFECmAW_ej_voli2VrvjuWo17yJ50Q",
@@ -39,7 +40,8 @@ const createStoreWithFirebase = compose(
 const rootReducer = combineReducers({
 	firebase: firebaseReducer,
 	firestore: firestoreReducer,
-	notify: notifyReducer
+	notify: notifyReducer,
+	settings: settingsReducer
 })
 
 // Crete initial state
