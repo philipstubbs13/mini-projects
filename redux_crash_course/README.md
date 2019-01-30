6f98810bbabdeef957116b834244f0f2dff037a4
philipstubbs13 <philipstubbs13@gmail.com>
2019-01-30 06:06:42 -0600

    redux crash course with react

---
+++ redux_crash_course/README.md
@@ -0,0 +1,9 @@
+# Redux Crash Course With React
+
+<https://www.youtube.com/watch?v=93p3LxR9xfM>
+
+```bash
+npm i redux react-redux redux-thunk
+```
+
+* Redux DevTools chrome extension
---
+++ redux_crash_course/reduxexample/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
