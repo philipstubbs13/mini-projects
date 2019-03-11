aa57dbf2f1d513553a07c5ba83e98eda1fdd273a
philipstubbs13 <philipstubbs13@gmail.com>
2019-03-10 21:55:21 -0500

    react and apollo

--- graphql_react_apollo/README.md
+++ graphql_react_apollo/README.md
@@ -1,3 +1,4 @@
 # GraphQL with React & Apollo
 
 * <https://www.youtube.com/watch?v=SEMTj8w04Z8&t=204s>
+* <https://www.youtube.com/watch?v=-XwkFm5a9lw>
---
+++ graphql_react_apollo/spacex_launch_stats/client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+import Launches from './components/Launches';
+import './App.css';
+import logo from './logo.jpg';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+          <Launches />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
