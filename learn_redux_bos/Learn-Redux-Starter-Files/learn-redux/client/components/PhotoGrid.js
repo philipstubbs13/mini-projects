403b47c9052c494ba5a03999a7e2abd24f5734a0
philipstubbs13 <philipstubbs13@gmail.com>
2019-03-14 02:55:10 -0500

    learn redux

---
+++ learn_redux_bos/Learn-Redux-Starter-Files/learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div classname="photo-grid">
+        I'm the photo grid
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid;
---
+++ learn_redux_bos/Learn-Redux-Starter-Files/learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div classname="single-photo">
+        I'm the single.
+      </div>
+    )
+  }
+})
+
+export default Single;
