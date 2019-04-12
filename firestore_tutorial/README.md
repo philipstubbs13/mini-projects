c300190a72bc7c51749716e5276ae78f9d399eba
philipstubbs13 <philipstubbs13@gmail.com>
2019-04-11 22:45:40 -0500

    firestore tut

--- firestore_tutorial/README.md
+++ firestore_tutorial/README.md
@@ -1,5 +1,6 @@
 # Firebase Firestore Tutorial
 
-* <https://www.youtube.com/watch?v=4d-gIPGzmK4>
+* <https://www.youtube.com/watch?v=UZqXcoqC95E&list=PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB&index=1>
 * Firestore is a real-time database.
 * Firestore is a NoSQL database.
+* Github repository: <https://github.com/iamshaunjp/firebase-firestore-playlist>
---
+++ firestore_tutorial/firebase_firestore/app.js
@@ -0,0 +1,94 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
+// create element and render cafe
+function renderCafe(doc) {
+  let li = document.createElement('li');
+  let name = document.createElement('span');
+  let city = document.createElement('span');
+  let cross = document.createElement('div');
+
+  li.setAttribute('data-id', doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+  cross.textContent = 'x';
+
+  li.appendChild(name);
+  li.appendChild(city);
+  li.appendChild(cross);
+
+  cafeList.appendChild(li);
+
+  // deleting data
+  cross.addEventListener('click', (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    db.collection('cafes').doc(id).delete();
+  })
+}
+
+// getting data
+// db.collection('cafes').get().then((snapshot) => {
+//   // console.log(snapshot.docs);
+//   snapshot.docs.forEach(doc => {
+//     // console.log(doc.data());
+//     renderCafe(doc);
+//   })
+// });
+
+// db.collection('cafes').where('city', '<', 'n').get().then((snapshot) => {
+//   // console.log(snapshot.docs);
+//   snapshot.docs.forEach(doc => {
+//     // console.log(doc.data());
+//     renderCafe(doc);
+//   })
+// });
+
+// db.collection('cafes').where('city','==', 'manchester').orderBy('name').get().then((snapshot) => {
+//   // console.log(snapshot.docs);
+//   snapshot.docs.forEach(doc => {
+//     // console.log(doc.data());
+//     renderCafe(doc);
+//   })
+// });
+
+// saving data
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('cafes').add({
+    name: form.name.value,
+    city: form.city.value
+  });
+  form.name.value = '';
+  form.city.value = '';
+});
+
+// real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  // console.log(changes);
+  changes.forEach(change => {
+    // console.log(change.doc.data());
+    if(change.type == 'added') {
+      renderCafe(change.doc);
+    } else if (change.type == 'removed'){
+      let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+      cafeList.removeChild(li);
+    }
+  })
+})
+
+// updating data
+// db.collection('cafes').doc('xMAfeMD0U6qCMVjRNtf3').update({
+//   name: 'Wario World'
+// })
+
+// db.collection('cafes').doc('xMAfeMD0U6qCMVjRNtf3').update({
+//   city: 'New York'
+// })
+
+// set completely overwrites document with new properties.
+// db.collection('cafes').doc('xMAfeMD0U6qCMVjRNtf3').set({
+//   name: 'shauns cafe',
+//   city: 'Liverpool'
+// })
