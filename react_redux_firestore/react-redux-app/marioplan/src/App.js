c8e5ed18a32513d9bb5af36e6a2a7969e63af165
philipstubbs13 <philipstubbs13@gmail.com>
2019-04-16 21:31:39 -0500

    react redux

--- react_redux_firestore/react-redux-app/marioplan/src/App.js
+++ react_redux_firestore/react-redux-app/marioplan/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
+import Signin from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -10,7 +13,10 @@ class App extends Component {
         <div className="App">
          <Navbar />
          <Switch>
-           <Route path="/" component={Dashboard} />
+           <Route exact path="/" component={Dashboard} />
+           <Route path='/project/:id' component={ProjectDetails} />
+           <Route path="/signin" component={Signin} />
+           <Route path="/signup" component={SignUp} />
          </Switch>
         </div>
       </BrowserRouter>
