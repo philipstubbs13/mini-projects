80279c02648480189e9a6c839386c157feb6c2c5
philipstubbs13 <philipstubbs13@gmail.com>
2019-05-05 12:49:10 -0500

    react registration

--- learn_mern_stack/mern_shopping_list/client/src/actions/authActions.js
+++ learn_mern_stack/mern_shopping_list/client/src/actions/authActions.js
@@ -29,6 +29,38 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
+// Register User
+export const register = ({ name, email, password }) => dispatch => {
+  // headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  // Request body
+  const body = JSON.stringify({ name, email, password });
+
+  axios.post('/api/users', body, config)
+    .then(res => dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
+      dispatch({
+        type: REGISTER_FAIL
+      });
+    });
+}
+
+// Logout user
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  }
+}
+
 // Setup config/headers and token
 export const tokenConfig = getState => {
   // Get token from local storage.
