bef0e16944485044211ec71c320b01398e9a98da
philipstubbs13 <philipstubbs13@gmail.com>
2019-05-05 13:21:55 -0500

    react login and access control

--- learn_mern_stack/mern_shopping_list/client/src/actions/authActions.js
+++ learn_mern_stack/mern_shopping_list/client/src/actions/authActions.js
@@ -29,7 +29,7 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
-// Register User
+// Register user
 export const register = ({ name, email, password }) => dispatch => {
   // headers
   const config = {
@@ -50,6 +50,31 @@ export const register = ({ name, email, password }) => dispatch => {
       dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
       dispatch({
         type: REGISTER_FAIL
+      });
+    });
+}
+
+// Login User
+export const login = ({ email, password }) => dispatch => {
+  // headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  // Request body
+  const body = JSON.stringify({ email, password });
+
+  axios.post('/api/auth', body, config)
+    .then(res => dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL'));
+      dispatch({
+        type: LOGIN_FAIL
       });
     });
 }
