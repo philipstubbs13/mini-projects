1a3b82235ef562ddc7c7f6f944becb6c662478be
philipstubbs13 <philipstubbs13@gmail.com>
2019-07-06 11:20:48 -0500

    real time chat app

---
+++ realtime_chat_node/README.md
@@ -0,0 +1,3 @@
+# Build a Real Time Chat App With Node.js ad Socket.io
+
+* <https://www.youtube.com/watch?v=rxzOqP9YwmM>
---
+++ realtime_chat_node/script.js
@@ -0,0 +1,36 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('send-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  console.log(`${data.name}: ${data.message}`)
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  console.log(name)
+  appendMessage(`${name} connected`)
+})
+
+socket.on('disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
