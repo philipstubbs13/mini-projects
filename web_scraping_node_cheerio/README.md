71154b6f3e3fa7c41492f276e1291203715096b3
philipstubbs13 <philipstubbs13@gmail.com>
2019-07-13 11:22:23 -0500

    web scraping

---
+++ web_scraping_node_cheerio/README.md
@@ -0,0 +1,3 @@
+# Intro to Web Scraping With Node.js and Cheerio
+
+* <https://www.youtube.com/watch?v=LoziivfAAjE>
---
+++ web_scraping_node_cheerio/scrape2.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv');
+
+// Write Headers
+writeStream.write(`Title,Link,Date \n`);
+
+request('http://codedemos.com/sampleblog', (error, response, html) => {
+  if(!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+
+    $('.post-preview').each((i, el) => {
+      const title = $(el).find('.post-title').text().replace(/\s\s+/g, '');
+      const link = $(el).find('a').attr('href');
+      const data = $(el).find('.post-date').text().replace(/,/, '')
+
+      console.log(title, link, date);
+      // Write Row To CSV
+      writeStream.write(`${title}, ${link}, ${date} \n`);
+    });
+
+    console.log('Scraping Done...');
+  }
+})
