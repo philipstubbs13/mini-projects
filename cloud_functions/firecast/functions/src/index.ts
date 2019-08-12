c53304caac026fb73d11e10e4ceef9e3d5d16dae
philipstubbs13 <philipstubbs13@gmail.com>
2019-08-11 23:13:18 -0500

    js promises

---
+++ cloud_functions/firecast/functions/src/index.ts
@@ -0,0 +1,22 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const getBostonWeather = functions.https.onRequest((request, response) => {
+  admin.firestore().doc('cities-weather/boston-ma-us').get()
+  .then(snapshot => {
+    const data = snapshot.data()
+    response.send(data);
+  })
+  .catch(error => {
+    // handle the error
+    console.log(error)
+    response.status(500).send(error)
+  })
+
+  // console.log('Hello');
+  // response.send("Hello from this Firecast!");
+});
