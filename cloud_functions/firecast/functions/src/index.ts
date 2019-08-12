79326875c82b962b8575e3ab75e3206627ca88ca
philipstubbs13 <philipstubbs13@gmail.com>
2019-08-12 07:25:02 -0500

    cloud functions

--- cloud_functions/firecast/functions/src/index.ts
+++ cloud_functions/firecast/functions/src/index.ts
@@ -5,6 +5,46 @@ admin.initializeApp()
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+export const getBostonAreaWeather =
+functions.https.onRequest((request, response) => {
+  admin.firestore().doc("areas/greater-boston").get()
+  .then(areaSnapshot => {
+    const cities = areaSnapshot.data().cities
+    const promises = []
+    for (const city in cities) {
+      const p = admin.firestore().doc(`cities-weather/${city}`).get()
+      promises.push(p)
+    }
+    return Promise.all(promises)
+  })
+  .then(citySnapshots => {
+    const results = [];
+    citySnapshots.forEach(citySnap => {
+      const data = citySnap.data()
+      data.city = citySnap.id
+      results.push(data)
+    })
+    response.send(results)
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(500).send(error)
+  })
+})
+
+
+export const onBostonWeatherUpdate =
+functions.firestore.document("cities-weather/boston-ma-us").onUpdate(change => {
+  const after = change.after.data()
+  const payload = {
+    data: {
+      temp: String(after.temp),
+      conditions: after.conditions
+    }
+  }
+  return admin.messaging().sendToTopic("weather_boston-ma-us", payload)
+})
+
 export const getBostonWeather = functions.https.onRequest((request, response) => {
   admin.firestore().doc('cities-weather/boston-ma-us').get()
   .then(snapshot => {
