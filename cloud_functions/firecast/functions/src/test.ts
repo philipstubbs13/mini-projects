a20be42f7967f9534f2c312da149cd605d555b67
philipstubbs13 <philipstubbs13@gmail.com>
2019-08-13 22:31:18 -0500

    async await

---
+++ cloud_functions/firecast/functions/src/test.ts
@@ -0,0 +1,12 @@
+async function myFunction(): Promise<string> {
+  try {
+    const rank = await getRank()
+    return "firebase is #" + rank
+  } catch (err) {
+    return "Error: " + err
+  }
+}
+
+function getRank() {
+  return Promise.resolve(1)
+}
