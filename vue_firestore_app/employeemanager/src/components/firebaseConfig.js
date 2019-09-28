bc300ca88d1cb6cc273c207a185596d2bdb2c59e
philipstubbs13 <philipstubbs13@gmail.com>
2019-09-28 11:46:24 -0500

    vue firestore

---
+++ vue_firestore_app/employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDiJPiWya39SFwjuoSCPj7HmUV0uhC3C2I",
+  authDomain: "vuefs-prod-bf8c6.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-bf8c6.firebaseio.com",
+  projectId: "vuefs-prod-bf8c6",
+  storageBucket: "vuefs-prod-bf8c6.appspot.com",
+  messagingSenderId: "756100976788",
+}
