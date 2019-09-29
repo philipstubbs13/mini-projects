7f028fe3e21bf21b93e19f56a008cad8d252a337
philipstubbs13 <philipstubbs13@gmail.com>
2019-09-29 14:18:36 -0500

    node

--- node_intro/node-js-playlist-master/app.js
+++ node_intro/node-js-playlist-master/app.js
@@ -1,1 +1,35 @@
-console.log('hey ninjas')
+// normal function statement
+// function sayHi() {
+//   console.log('hi')
+// }
+
+// sayHi();
+
+// function callFunction(fun) {
+//   fun();
+// }
+
+// function expression
+// var sayBye = function(){
+//   console.log('bye');
+// };
+
+// callFunction(sayBye);
+
+// console.log('hey ninjas');
+
+// setTimeout(function(){
+//   console.log('3 seconds have passed.');
+// }, 3000);
+
+// var time = 0;
+// var timer = setInterval(function(){
+//   time += 2;
+//   console.log(time + ' seconds have passed.');
+//   if (time > 5) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
+
+// console.log(__dirname);
+// console.log(__filename);
