d7eb872d6af8643967200fc398e9a4bc33e18ec8
philipstubbs13 <philipstubbs13@gmail.com>
2019-12-31 08:44:02 -0600

    graphql

--- graphql_with_react_tutorial/graphql-playlist/server/schema/schema.js
+++ graphql_with_react_tutorial/graphql-playlist/server/schema/schema.js
@@ -1,13 +1,42 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
+
+// dummy data
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+]
+
+var authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1'},
+  { name: 'Brandon Sanderson', age: 42, id: '2'},
+  { name: 'Terry Pratchett', age: 66, id: '3'}
+]
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -16,9 +45,18 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString }},
+      args: { id: { type: GraphQLID }},
       resolve(parent, args) {
         // code to get data from db/other source.
+        // console.log(typeof(args.id))
+        return _.find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id })
       }
     }
   }
@@ -27,3 +65,17 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
   query: RootQuery
 })
+
+// {
+//   book(id: "1") {
+//     name
+//     genre
+//   }
+// }
+
+// {
+//   author(id: 1) {
+//     name
+//   	age
+//   }
+// }
