c8db8dbceafe3da557146ba273f6f889002733fb
philipstubbs13 <philipstubbs13@gmail.com>
2020-01-01 01:14:41 -0600

    stuck

--- graphql_with_react_tutorial/graphql-playlist/server/app.js
+++ graphql_with_react_tutorial/graphql-playlist/server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database.
+mongoose.connect("mongodb://phil123:phil123@ds139946.mlab.com:39946/gql-ninja", { useNewUrlParser: true } );
+mongoose.connection.once("open", () => {
+  console.log('connected to database.')
+})
 
 app.use("/graphql", graphqlHTTP({
   schema,
