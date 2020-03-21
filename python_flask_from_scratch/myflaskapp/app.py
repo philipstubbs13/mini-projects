c743ecec91c0dbcc8118599c6865129f535ba83f
philipstubbs13 <philipstubbs13@gmail.com>
2020-03-20 20:09:44 -0500

    flaskapp

--- python_flask_from_scratch/myflaskapp/app.py
+++ python_flask_from_scratch/myflaskapp/app.py
@@ -211,6 +211,67 @@ def add_article():
 
   return render_template('add_article.html', form=form)
 
+# Edit Article
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+  # Create cursor
+  cur = mysql.connection.cursor()
+
+  # Get article by id
+  result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+  article = cur.fetchone()
+
+  # Get form
+  form = ArticleForm(request.form)
+
+  # Populate article form fields
+  form.title.data = article['title']
+  form.body.data = article['body']
+
+  if request.method == 'POST' and form.validate():
+    title = request.form['title']
+    body = request.form['body']
+
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Execute
+    cur.execute("UPDATE articles SET title=%s, body=%s WHERE id = %s", (title, body, id))
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    # Close connection
+    cur.close()
+
+    flash('Article Updated', 'success')
+
+    return redirect(url_for('dashboard'))
+
+  return render_template('edit_article.html', form=form)
+
+# Delete article
+@app.route('/delete_article/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_article(id):
+  # Create cursor
+  cur = mysql.connection.cursor()
+
+  # Execute
+  cur.execute("DELETE FROM articles WHERE id = %s", [id])
+
+  # Commit to DB
+  mysql.connection.commit()
+
+  # Close connection
+  cur.close()
+
+  flash('Article Deleted', 'success')
+
+  return redirect(url_for('dashboard'))
+
 if __name__ == '__main__':
   app.secret_key='secret123'
   app.run(debug=True)
