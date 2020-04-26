9c0e469f4bb46b320918c8443e5b07b367409ef4
philipstubbs13 <philipstubbs13@gmail.com>
2020-04-26 13:41:14 -0500

    brainjs

---
+++ machine_learning_with_javascript/README.md
@@ -0,0 +1,6 @@
+# Simple Machine Learning With JavaScript - Brain.js
+
+* <https://www.youtube.com/watch?v=RVMHhtTqUxc&t=171s>
+* <https://github.com/bradtraversy/brainjs_examples>
+* <https://coderoncode.com/machine/learning/2016/06/06/machine-learning-a-simple-neural-network.html>
+* <https://itnext.io/you-can-build-a-neural-network-in-javascript-even-if-you-dont-really-understand-neural-networks-e63e12713a3>
---
+++ machine_learning_with_javascript/brainjs_examples/index2.js
@@ -0,0 +1,18 @@
+const brain = require('brain.js');
+const data = require('./data.json');
+
+const network = new brain.recurrent.LSTM();
+
+const trainingData = data.map(item => ({
+  input: item.text,
+  output: item.category
+}));
+
+network.train(trainingData, {
+  iterations: 2000
+});
+
+// const output = network.run('I fixed the power supply');
+const output = network.run('The code has some bugs');
+
+console.log(`Category: ${output}`)
