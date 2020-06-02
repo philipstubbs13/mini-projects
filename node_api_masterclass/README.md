dbb5d18999e8657b566ae1eccbcdcce5d98aa789
philipstubbs13 <philipstubbs13@gmail.com>
2020-06-01 21:30:12 -0500

    http intro

---
+++ node_api_masterclass/README.md
@@ -0,0 +1,19 @@
+# Node.js API Masterclass With Express and MongoDB
+
+* <https://www.udemy.com/course/nodejs-api-masterclass/learn/lecture/16581658#overview>
+
+* Status codes
+  * <https://developer.mozilla.org/en-US/docs/Web/HTTP/Status>
+* HTTP Request Methods
+  * <https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods>
+* Restful API Standards
+  * GET /todos
+    * Get todos
+  * GET /todos/1
+    * Get todo with ID of 1
+  * POST /todos
+    * Add a todo
+  * PUT /todos/1
+    * Update todo with ID of 1
+  * DELETE /todos/1
+    * Delete todo with ID of 1.
---
+++ node_api_masterclass/nodehttp/server.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+const todos = [
+  { id: 1, text: 'Todo One'},
+  { id: 2, text: 'Todo Two'},
+  { id: 3, text: 'Todo Three'},
+]
+
+const server = http.createServer((req, res) => {
+  // res.statusCode = 404;
+  // res.setHeader('Content-Type', 'application/json');
+  // res.setHeader('X-Powered-By', 'Node.js');
+  // res.write('<h1>Hello</h1>');
+  // res.write('<h2>Hello Again</h2>')
+
+  console.log(req.headers.authorization);
+  const { method, url } = req;
+  let body = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString()
+
+    let status = 404;
+    const response = {
+      success: false,
+      data: null,
+      error: null,
+    }
+
+    if (method === 'GET' && url === '/todos') {
+      status = 200;
+      response.success = true;
+      response.data = todos
+    } else if (method === 'POST' && url === '/todos') {
+      const { id, text } = JSON.parse(body);
+
+      if(!id || !text) {
+        status = 400
+        response.error = 'please add id and text'
+      } else {
+        todos.push({ id, text })
+        status = 201;
+        response.success = true;
+        response.data = todos;
+      }
+    }
+
+    res.writeHead(status, {
+      'Content-Type': 'application/json',
+      'X-Powered-By': 'Node.js'
+    });
+
+    res.end(
+      JSON.stringify(response)
+    )
+  })
+});
+
+const PORT = 5000;
+
+server.listen(PORT,() => console.log(`Server running on port ${PORT}.`));
