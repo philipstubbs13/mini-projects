56acbf929382cbf1f8020f1f584ec6fb35c89305
philipstubbs13 <philipstubbs13@gmail.com>
2020-06-06 09:11:58 -0500

    model

--- node_api_masterclass/devcamper_api/server.js
+++ node_api_masterclass/devcamper_api/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -22,4 +27,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}.`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}.`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+})
