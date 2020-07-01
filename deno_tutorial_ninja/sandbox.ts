5d274e29dc30b41ddf643caee930df52f4c4219c
philipstubbs13 <philipstubbs13@gmail.com>
2020-07-01 05:21:43 -0500

    third party modules

--- deno_tutorial_ninja/sandbox.ts
+++ deno_tutorial_ninja/sandbox.ts
@@ -68,6 +68,19 @@ const server = serve({ port: 3000 });
 console.log('listening for requests on port 3000');
 
 for await (const req of server) {
+  console.log('request made');
   const url = req.url;
-  req.respond({ body: `Hello ninjas, you visited ${url}` })
+  req.respond({ body: `Hello again, ninjas, you visited ${url}` })
 }
+
+// case third party module
+import { camelCase, paramCase, pascalCase, snakeCase } from "https://deno.land/x/case/mod.ts";
+
+const text = 'hello again ninjas';
+
+const camel = camelCase(text);
+const param = paramCase(text);
+const pascal = pascalCase(text);
+const snake = snakeCase(text);
+
+console.log(camel, param, pascal, snake);
