6ba77fb724c50061b579535e0f07520832806904
philipstubbs13 <philipstubbs13@gmail.com>
2020-07-03 14:37:31 -0500

    api

--- deno_tutorial_ninja/app.ts
+++ deno_tutorial_ninja/app.ts
@@ -1,4 +1,10 @@
 import { Application, Context } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
+import {
+  get_all_books,
+  get_book,
+  create_book,
+  delete_book
+ } from './controllers/bookController.ts';
 
 const app = new Application();
 
@@ -9,6 +15,12 @@ app.static('/', './public');
 app.get('/', async (ctx: Context) => {
   await ctx.file('./public/index.html');
 })
+
+app
+  .get('/books', get_all_books)
+  .get('/books/:id', get_book)
+  .post('/books', create_book)
+  .delete('/books/:id', delete_book)
 
 // listen to port
 app.start({ port: 3000 });
