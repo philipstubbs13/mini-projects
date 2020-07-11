8e633b4d95262ef0e098190d23c3beb77d9a2b04
philipstubbs13 <philipstubbs13@gmail.com>
2020-07-11 07:30:17 -0500

    server template setup

---
+++ deno_websockets_tutorial/deno_websockets/app.ts
@@ -0,0 +1,16 @@
+import { serve } from "https://deno.land/std/http/server.ts"; 
+
+// setup server
+const server = serve({ port: 3000 });
+console.log("http://localhost:3000")
+
+for await (const req of server) {
+
+  // serve index page
+  if (req.url === "/") {
+    req.respond({
+      status: 200,
+      body: await Deno.open('./public/index.html')
+    })
+  }
+}
