c3b940e282c21708ac014e3f41cf3de22f2a4fb6
philipstubbs13 <philipstubbs13@gmail.com>
2020-07-19 11:50:59 -0500

    foor loops

---
+++ automate_boring_stuff_with_python/fivetimes.py
@@ -0,0 +1,13 @@
+print('My name is')
+# i = 0
+# while i < 5:
+for i in range(5, -1, -1):
+  print('Jimmy Five Times ' + str(i))
+  i = i + 1
+
+# total = 0
+# for num in range(101):
+#   total = total + num
+# print(total)
+
+  
