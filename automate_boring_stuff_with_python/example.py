7b9d58fff0789b7df4904917bfe084d3f0e2743b
philipstubbs13 <philipstubbs13@gmail.com>
2020-07-20 19:25:35 -0500

    functions

---
+++ automate_boring_stuff_with_python/example.py
@@ -0,0 +1,19 @@
+# def hello(name):
+#   print('Hello ' + name)
+
+# hello('Alice')
+# hello('Bob')
+
+
+
+
+# def plusOne(number):
+#   return number + 1
+
+# newNumber = plusOne(5)
+# print(newNumber)
+
+
+# print('Hello', end='')
+# print('World')
+# print('cat', 'dog', 'mouse', sep='ABC')
