092336ab296d55c7be25a35d8aacc5e5e056d96f
philipstubbs13 <philipstubbs13@gmail.com>
2020-07-26 13:15:22 -0500

    lists strings

--- automate_boring_stuff_with_python/README.md
+++ automate_boring_stuff_with_python/README.md
@@ -1,3 +1,4 @@
 # Automate the Boring Stuff with Python
 
 * <https://www.udemy.com/course/automate/learn/lecture/3309062#overview>
+* Strings in python are immutable, they cannot be changed.
---
+++ automate_boring_stuff_with_python/strings_vs_lists.py
@@ -0,0 +1,15 @@
+# list('Hello')
+
+# name = 'Zophie'
+# name[0]
+# name[1:3]
+# name[-2]
+# 'Zo' in name
+# 'xxx' in name
+
+def eggs(someParameter):
+  someParameter.append('Hello')
+
+spam = [1, 2, 3]
+eggs(spam)
+print(spam)
