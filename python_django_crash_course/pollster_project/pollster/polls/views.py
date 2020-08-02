70577f15eccf2d3fbf3236d2ee33814a1065434f
philipstubbs13 <philipstubbs13@gmail.com>
2020-08-02 14:19:26 -0500

    python django

---
+++ python_django_crash_course/pollster_project/pollster/polls/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .models import Question, Choice
+
+# Get questions and display them
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
+
+# Show specific question and choices
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/results.html', { 'question': question })
+
+# Get question and display results
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
