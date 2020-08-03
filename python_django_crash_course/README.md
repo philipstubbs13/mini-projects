efa787b1824ecf0dce0ce8165001c5774b401257
philipstubbs13 <philipstubbs13@gmail.com>
2020-08-02 21:40:41 -0500

    django

---
+++ python_django_crash_course/README.md
@@ -0,0 +1,3 @@
+# Python Django Crash Course
+
+* <https://www.youtube.com/watch?v=e1IyzVyrLSU&t=5s>
--- python_django_crash_course/pollster_project/pollster/polls/views.py
+++ python_django_crash_course/pollster_project/pollster/polls/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -14,9 +17,29 @@ def detail(request, question_id):
     question = Question.objects.get(pk=question_id)
   except Question.DoesNotExist:
     raise Http404("Question does not exist")
-  return render(request, 'polls/results.html', { 'question': question })
+  return render(request, 'polls/detail.html', { 'question': question })
 
 # Get question and display results
 def results(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/results.html', { 'question': question })
+
+# Vote for a question choice
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
