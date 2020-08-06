939e54d7906c802346e7a5184e5278cd6bc28257
philipstubbs13 <philipstubbs13@gmail.com>
2020-08-06 06:38:51 -0500

    query variables

--- react_query/starwars/src/components/Planets.js
+++ react_query/starwars/src/components/Planets.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets');
+const fetchPlanets = async (key, greeting, page) => {
+  console.log(greeting);
+  const res = await fetch(`http://swapi.dev/api/planets?page=${page}`);
   return res.json();
 
 }
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets, {
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(['planets', 'hello, ninjas', page], fetchPlanets, {
     staleTime: 0,
     // cacheTime: 10,
     onSuccess: () => console.log('data fetched with no problemo')
@@ -20,6 +22,10 @@ const Planets = () => {
     <div>
       <h2>Planets</h2>
       {/* <p>{ status }</p> */}
+
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
 
       {status === 'loading' && (
         <div>Loading data</div>
