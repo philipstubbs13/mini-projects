e738cedf9810e4a36c69a3cd040cc53738b4f8ec
philipstubbs13 <philipstubbs13@gmail.com>
2020-09-15 23:57:37 -0500

    sapper starter

---
+++ sapper_tutorial/job-ninja/src/routes/about.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>About Job Ninja</h1>
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, labore,
+  laborum dolorum nulla voluptatum totam aspernatur minus consequuntur
+  accusantium minima odit quidem voluptatem sapiente itaque natus numquam ipsa,
+  quam officiis!
+</p>
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, labore,
+  laborum dolorum nulla voluptatum totam aspernatur minus consequuntur
+  accusantium minima odit quidem voluptatem sapiente itaque natus numquam ipsa,
+  quam officiis!
+</p>
---
+++ sapper_tutorial/job-ninja/src/routes/contact.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>Contact</title>
+</svelte:head>
+
+<h1>Contact Job Ninja</h1>
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, labore,
+  laborum dolorum nulla voluptatum totam aspernatur minus consequuntur
+  accusantium minima odit quidem voluptatem sapiente itaque natus numquam ipsa,
+  quam officiis!
+</p>
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, labore,
+  laborum dolorum nulla voluptatum totam aspernatur minus consequuntur
+  accusantium minima odit quidem voluptatem sapiente itaque natus numquam ipsa,
+  quam officiis!
+</p>
