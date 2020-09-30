89fa2e67172eea2e22f6530d549bd44977aa4b53
philipstubbs13 <philipstubbs13@gmail.com>
2020-09-29 22:52:30 -0500

    hashing passwords

--- node_auth_jwt/node-express-jwt-auth/models/User.js
+++ node_auth_jwt/node-express-jwt-auth/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -24,8 +25,9 @@ userSchema.post('save', function (doc, next) {
 });
 
 // fire a function before doc saved to db.
-userSchema.pre('save', function(next) {
-  console.log('user about to be created and saved', this);
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
