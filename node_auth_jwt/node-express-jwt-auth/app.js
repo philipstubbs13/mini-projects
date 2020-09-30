a2e3e9033dea21a1399e7ddd8b583fa196146f7f
philipstubbs13 <philipstubbs13@gmail.com>
2020-09-29 22:12:12 -0500

    mongoose validation

--- node_auth_jwt/node-express-jwt-auth/app.js
+++ node_auth_jwt/node-express-jwt-auth/app.js
@@ -6,13 +6,13 @@ const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json);
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://:@cluster0.dt97d.mongodb.net/node-auth';;
+const dbURI = 'mongodb+srv://:@cluster0.dt97d.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
