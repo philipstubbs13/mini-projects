2a9778c55641d3e76677889ae42a70a43c326e00
philipstubbs13 <philipstubbs13@gmail.com>
2020-10-21 05:54:39 -0500

    callback hell

--- async_javascript_tutorial/sandbox.js
+++ async_javascript_tutorial/sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener('readystatechange', () => {
@@ -11,20 +11,21 @@ const getTodos = (callback) => {
     }
   })
 
-  request.open('GET', 'todos.json');
+  request.open('GET', resource);
   request.send();
 };
 
 console.log(1);
 console.log(2);
 
-getTodos((err, data) => {
-  console.log('callback fired');
-  if (err) {
-    console.log(err);
-  } else {
+getTodos('todos/luigi.json', (err, data) => {
+  console.log(data);
+  getTodos('todos/mario.json', (err, data) => {
     console.log(data);
-  }
+    getTodos('todos/shaun.json', (err, data) => {
+      console.log(data);
+    });
+  });
 });
 
 console.log(3);
