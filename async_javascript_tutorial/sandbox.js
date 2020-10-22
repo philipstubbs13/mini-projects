4bdd1902482df584f1637c01a68c93bb0ec8526f
philipstubbs13 <philipstubbs13@gmail.com>
2020-10-22 06:16:42 -0500

    throwing errors

--- async_javascript_tutorial/sandbox.js
+++ async_javascript_tutorial/sandbox.js
@@ -2,6 +2,11 @@
 
 const getTodos = async () => {
   const response = await fetch('todos/luigi.json');
+
+  if (response.status !== 200) {
+    throw new Error('cannot fetch the data');
+  }
+
   const data = await response.json();
 
   return data;
@@ -12,6 +17,7 @@ console.log(2);
 
 getTodos()
 .then(data => console.log('resolved', data))
+.catch(err => console.log('rejected', err.message));
 
 console.log(3);
 console.log(4);
