270c4a4246f8ae77e37b8f96cd802b4912e7ba0a
philipstubbs13 <philipstubbs13@gmail.com>
2020-10-22 05:48:01 -0500

    promise chaining

--- async_javascript_tutorial/sandbox.js
+++ async_javascript_tutorial/sandbox.js
@@ -19,7 +19,13 @@ const getTodos = (resource) => {
 };
 
 getTodos('todos/luigi.json').then(data => {
-  console.log('promise resolved:', data);
+  console.log('promise 1 resolved:', data);
+  return getTodos('todos/mario.json');
+}).then(data => {
+  console.log('promise 2 resolved: ', data);
+  return getTodos('todos/shaun.json');
+}).then(data => {
+  console.log('promise 3 resolved: ', data);
 }).catch((err) => {
   console.log('promise rejected:', err);
 })
