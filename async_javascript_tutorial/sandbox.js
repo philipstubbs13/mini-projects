b1733c69938773d92f4cc0cd40cfa6f2463321d4
philipstubbs13 <philipstubbs13@gmail.com>
2020-10-22 05:57:40 -0500

    fetch api

--- async_javascript_tutorial/sandbox.js
+++ async_javascript_tutorial/sandbox.js
@@ -1,37 +1,48 @@
-const getTodos = (resource) => {
+// fetch api
 
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-      // console.log(request, request.readyState);
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4 ) {
-        reject('error getting resource');
-      }
-    })
-
-    request.open('GET', resource);
-    request.send();
-  });
-};
-
-getTodos('todos/luigi.json').then(data => {
-  console.log('promise 1 resolved:', data);
-  return getTodos('todos/mario.json');
+fetch('todos/luigi.json').then((response) => {
+  console.log('resolved', response);
+  return response.json();
 }).then(data => {
-  console.log('promise 2 resolved: ', data);
-  return getTodos('todos/shaun.json');
-}).then(data => {
-  console.log('promise 3 resolved: ', data);
+  console.log(data);
 }).catch((err) => {
-  console.log('promise rejected:', err);
-})
+  console.log('rejected', err);
+});
 
-console.log(1);
-console.log(2);
+// const getTodos = (resource) => {
+
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
+
+//     request.addEventListener('readystatechange', () => {
+//       // console.log(request, request.readyState);
+//       if (request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if (request.readyState === 4 ) {
+//         reject('error getting resource');
+//       }
+//     })
+
+//     request.open('GET', resource);
+//     request.send();
+//   });
+// };
+
+// getTodos('todos/luigi.json').then(data => {
+//   console.log('promise 1 resolved:', data);
+//   return getTodos('todos/mario.json');
+// }).then(data => {
+//   console.log('promise 2 resolved: ', data);
+//   return getTodos('todos/shaun.json');
+// }).then(data => {
+//   console.log('promise 3 resolved: ', data);
+// }).catch((err) => {
+//   console.log('promise rejected:', err);
+// })
+
+// console.log(1);
+// console.log(2);
 
 // getTodos('todos/luigi.json', (err, data) => {
 //   console.log(data);
@@ -43,8 +54,8 @@ console.log(2);
 //   });
 // });
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
 
 
 // promise example
