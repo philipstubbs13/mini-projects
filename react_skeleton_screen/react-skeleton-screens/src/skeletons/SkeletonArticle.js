eba65a7b2659ee62f5b43f9984d809d75cc68663
philipstubbs13 <philipstubbs13@gmail.com>
2020-10-26 21:27:32 -0500

    animations

--- react_skeleton_screen/react-skeleton-screens/src/skeletons/SkeletonArticle.js
+++ react_skeleton_screen/react-skeleton-screens/src/skeletons/SkeletonArticle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SkeletonElement from './SkeletonElement';
+import Shimmer from './Shimmer';
 
 const SkeletonArticle = ({ theme }) => {
   const themeClass = theme || 'light';
@@ -12,6 +13,7 @@ const SkeletonArticle = ({ theme }) => {
         <SkeletonElement type='text' />
         <SkeletonElement type='text' />
       </div>
+      <Shimmer />
     </div>
   )
 }
