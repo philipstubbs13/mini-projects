4f118c61cca213e30addcf0b9af2ef996b5845f4
philipstubbs13 <philipstubbs13@gmail.com>
2020-10-26 21:03:48 -0500

    skeleton profile

--- react_skeleton_screen/react-skeleton-screens/src/components/User.js
+++ react_skeleton_screen/react-skeleton-screens/src/components/User.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import SkeletonProfile from '../skeletons/SkeletonProfile';
 
 const User = () => {
   const [profile, setProfile] = useState(null);
@@ -22,7 +23,7 @@ const User = () => {
         </div>
       )}
 
-      {!profile && <div>Loading...</div>}
+      {!profile && <SkeletonProfile />}
     </div>
   )
 }
