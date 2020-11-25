276680ec8d5dd08a2a58f79dc3854f255b00af9c
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-25 01:49:38 -0600

    regex groups

--- automate_boring_stuff_with_python/regular_expressions.py
+++ automate_boring_stuff_with_python/regular_expressions.py
@@ -1,10 +1,24 @@
 import re
 
-message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999'
+# message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999'
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.findall(message)
-print(mo)
+# phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# mo = phoneNumRegex.findall(message)
+# print(mo)
+
+# phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+# mo = phoneNumberRegex.search('My number is 415-555-4242')
+# print(mo.group())
+# print(mo.group(1))
+# print(mo.group(2))
+# phoneNumberRegex = re.compile(r'\(\d\d\d\) \d\d\d-\d\d\d\d')
+# mo = phoneNumberRegex.search('My number is (415)-555-4242')
+# print(mo.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1))
 
 # def isPhoneNumber(text):
 #   if len(text) != 12:
