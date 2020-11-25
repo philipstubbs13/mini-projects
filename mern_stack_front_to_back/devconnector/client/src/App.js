21a49a4cb53cee14edf587730d5930e6c929a075
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-25 02:48:10 -0600

    protected route

--- mern_stack_front_to_back/devconnector/client/src/App.js
+++ mern_stack_front_to_back/devconnector/client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -13,7 +15,7 @@ import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-if(localStorage.token) {
+if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
@@ -33,11 +35,13 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
       </Router>
     </Provider>
-)};
+  )
+};
 
 export default App;
