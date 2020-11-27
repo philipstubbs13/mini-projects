9558bab15c1b1e3b68dbcb852ddd4a7f22453db3
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-27 08:01:49 -0600

    delete profile

--- mern_stack_front_to_back/devconnector/client/src/components/profile-forms/AddEducation.js
+++ mern_stack_front_to_back/devconnector/client/src/components/profile-forms/AddEducation.js
@@ -58,7 +58,7 @@ const AddEducation = ({ addEducation, history }) => {
                 setFormData({ ...formData, current: !current });
                 toggleDisabled(!toDateDisabled);
               }} />
-            {' '}Current Job
+            {' '}Current School
           </p>
         </div>
         <div className="form-group">
--- mern_stack_front_to_back/devconnector/routes/api/profile.js
+++ mern_stack_front_to_back/devconnector/routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route         GET api/profile/me
 // @description   Get current users profile.
@@ -15,12 +16,12 @@ router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
-    if(!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user.'});
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user.' });
     }
 
     res.json(profile);
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
@@ -29,13 +30,13 @@ router.get('/me', auth, async (req, res) => {
 // @route         POST api/profile
 // @description   Create or update user profile.
 // @access        Private
-router.post('/', [ auth, [
+router.post('/', [auth, [
   check('status', 'Status is required.').not().isEmpty(),
   check('skills', 'Skills is required').not().isEmpty()
 ]], async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array( )});
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const {
@@ -56,13 +57,13 @@ router.post('/', [ auth, [
   // Build profile object
   const profileFields = {};
   profileFields.user = req.user.id;
-  if(company) profileFields.company = company;
-  if(website) profileFields.website = website;
-  if(location) profileFields.location = location;
-  if(bio) profileFields.bio = bio;
-  if(status) profileFields.status = status;
-  if(githubusername) profileFields.githubusername = githubusername;
-  if(skills) {
+  if (company) profileFields.company = company;
+  if (website) profileFields.website = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubusername) profileFields.githubusername = githubusername;
+  if (skills) {
     profileFields.skills = skills.split(',').map(skill => skill.trim());
   }
 
@@ -77,12 +78,12 @@ router.post('/', [ auth, [
   try {
     let profile = await Profile.findOne({ user: req.user.id });
 
-    if(profile) {
+    if (profile) {
       // Update
       profile = await Profile.findOneAndUpdate(
         { user: req.user.id },
         { $set: profileFields },
-        { new: true}
+        { new: true }
       );
 
       return res.json(profile);
@@ -93,7 +94,7 @@ router.post('/', [ auth, [
 
     await profile.save();
     res.json(profile);
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
@@ -138,14 +139,16 @@ router.get('/user/:user_id', async (req, res) => {
 // @access        Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
     // Remove user
     await User.findOneAndRemove({ _id: req.user.id })
 
-    res.json({ msg: 'User deleted'});
+    res.json({ msg: 'User deleted' });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
@@ -162,7 +165,7 @@ router.put('/experience', [auth, [
 
 ]], async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
@@ -233,7 +236,7 @@ router.put('/education', [auth, [
 
 ]], async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
