5e0530e440b208f08deae69694c0a5bb38eee265
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-27 15:10:16 -0600

    profiletop and profileabout

--- mern_stack_front_to_back/devconnector/client/src/components/profile/Profile.js
+++ mern_stack_front_to_back/devconnector/client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
   match,
@@ -13,7 +15,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, []);
+  }, [getProfileById, match.params.id]);
 
   return <Fragment>
     {profile === null || loading ? <Spinner /> : <Fragment>
@@ -22,8 +24,14 @@ const Profile = ({
       </Link>
       {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>
         Edit Profile
-      </Link>)}
-    </Fragment>}
+      </Link>
+      )}
+      <div class="profile-grid my-1">
+        <ProfileTop profile={profile} />
+        <ProfileAbout profile={profile} />
+      </div>
+    </Fragment>
+    }
   </Fragment>
 }
 
