6f9e42df42d1ceb260f4e61d98254116eadbd5a2
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-28 08:40:13 -0600

    adding comments

--- mern_stack_front_to_back/devconnector/client/src/components/post/CommentForm.js
+++ mern_stack_front_to_back/devconnector/client/src/components/post/CommentForm.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addPost } from '../../actions/post';
+import { addComment } from '../../actions/post';
 
-const PostForm = ({ addPost }) => {
+const CommentForm = ({ postId, addComment }) => {
   const [text, setText] = useState('');
 
   return (
     <div className="post-form">
       <div className="bg-primary p">
-        <h3>Say Something...</h3>
+        <h3>Leave A Comment</h3>
       </div>
       <form className="form my-1" onSubmit={e => {
         e.preventDefault();
-        addPost({ text });
+        addComment(postId, { text });
         setText('');
       }}>
         <textarea
           name="text"
           cols="30"
           rows="5"
-          placeholder="Create a post"
+          placeholder="Comment on this post"
           value={text}
           onChange={e => setText(e.target.value)}
           required
@@ -31,8 +31,8 @@ const PostForm = ({ addPost }) => {
   )
 }
 
-PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired,
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
 };
 
-export default connect(null, { addPost })(PostForm);
+export default connect(null, { addComment })(CommentForm);
