072ecee29c5a6dfc322dd77d350cb3f76148502b
philipstubbs13 <philipstubbs13@gmail.com>
2020-12-27 09:09:00 -0600

    flask html templates

---
+++ flask_creating_basic_website/flask_tutorial/tutorial1.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def home(name):
+  return render_template("index.html", content=["tim", "joe", "bill"])
+
+# @app.route("/<name>")
+# def user(name):
+#   return f"Hello {name}!"
+
+# @app.route("/admin/")
+# def admin():
+#   return redirect(url_for("user", name="Admin!"))
+
+if __name__ == "__main__":
+  app.run()
