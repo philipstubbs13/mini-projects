c45cd1dfc75169a9bd5d482b1d33eeeef9b1ef09
philipstubbs13 <philipstubbs13@gmail.com>
2020-12-27 19:28:21 -0600

    retrieving form data

--- flask_creating_basic_website/flask_tutorial/tutorial1.py
+++ flask_creating_basic_website/flask_tutorial/tutorial1.py
@@ -1,10 +1,26 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-  return render_template("index.html", content=["tim", "joe", "bill"])
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+  if request.method == "POST":
+    user = request.form["nm"]
+    return redirect(url_for("user", usr=user))
+  else:
+    return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+  return f"<h1>{usr}</h1>"
+
+@app.route("/test")
+def test():
+  return render_template("new.html")
 
 # @app.route("/<name>")
 # def user(name):
@@ -15,4 +31,4 @@ def home(name):
 #   return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
