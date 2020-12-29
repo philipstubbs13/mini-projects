e5780509841d1e64180b7fc76ac49e4addfa1201
philipstubbs13 <philipstubbs13@gmail.com>
2020-12-28 20:47:34 -0600

    page navigating and clicking elements

--- python_selenium/tutorial1.py
+++ python_selenium/tutorial1.py
@@ -11,26 +11,52 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://techwithtim.net")
-print(driver.title)
 
-search = driver.find_element_by_name("s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
-
-# print(driver.page_source)
+link = driver.find_element_by_link_text("Python Programming")
+link.click()
 
 try:
-  main = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "main"))
+  element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
   )
-  # print(main.text)
+  # element.clear()
+  element.click()
 
-  articles = main.find_elements_by_tag_name("article")
-  for article in articles:
-    header = article.find_element_by_class_name("entry-summary")
-    print(header.text)
-finally:
-  time.sleep(5)
+  element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+  )
+  element.click()
 
+  driver.back()
+  driver.back()
+  driver.back()
+  driver.forward()
+  driver.forward()
+
+except:
   driver.quit()
+
+
+# print(driver.title)
+
+# search = driver.find_element_by_name("s")
+# search.send_keys("test")
+# search.send_keys(Keys.RETURN)
+
+# # print(driver.page_source)
+
+# try:
+#   main = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.ID, "main"))
+#   )
+#   # print(main.text)
+
+#   articles = main.find_elements_by_tag_name("article")
+#   for article in articles:
+#     header = article.find_element_by_class_name("entry-summary")
+#     print(header.text)
+# finally:
+#   time.sleep(5)
+
+#   driver.quit()
 
