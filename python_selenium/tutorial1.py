8a344032783221b250a7d27fd426a397ff5f4788
philipstubbs13 <philipstubbs13@gmail.com>
2020-12-28 18:51:34 -0600

    locating elements from html python selenium

--- python_selenium/tutorial1.py
+++ python_selenium/tutorial1.py
@@ -1,10 +1,36 @@
 # Selenium Tutorial #1
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://techwithtim.net")
 print(driver.title)
-driver.quit()
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+# print(driver.page_source)
+
+try:
+  main = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "main"))
+  )
+  # print(main.text)
+
+  articles = main.find_elements_by_tag_name("article")
+  for article in articles:
+    header = article.find_element_by_class_name("entry-summary")
+    print(header.text)
+finally:
+  time.sleep(5)
+
+  driver.quit()
+
