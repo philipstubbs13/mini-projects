9dda2b8189af74e92eff4f1883cc2be370fb54c7
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-05 05:25:47 -0600

    header and footer components

--- svelte_for_beginners/ninjapolls/src/App.svelte
+++ svelte_for_beginners/ninjapolls/src/App.svelte
@@ -1,10 +1,17 @@
 <script>
-
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
 </script>
 
+<Header />
 <main>
-	<h1>Hello, Ninjas!</h1>
+
 </main>
+<Footer />
 
 <style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
