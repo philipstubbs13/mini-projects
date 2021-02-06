2ca53e2fcfbb870aae0a210fae3f306ec627f1c1
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-06 11:01:56 -0600

    introduction to stores

--- svelte_for_beginners/ninjapolls/src/App.svelte
+++ svelte_for_beginners/ninjapolls/src/App.svelte
@@ -12,17 +12,6 @@
     activeItem = e.detail;
   }
 
-  let polls = [
-    {
-      id: 1,
-      question: 'Python or JavaScript?',
-      answerA: 'Python',
-      answerB: 'JavaScript',
-      votesA: 9,
-      votesB: 15,
-    }
-  ];
-
   const handleAdd = (e) => {
     const poll = e.detail;
     polls = [poll, ...polls];
@@ -51,7 +40,7 @@
 <main>
   <Tabs {activeItem} {items} on:tabChange={tabChange} />
   {#if activeItem === 'Current Polls'}
-    <PollList {polls} on:vote={handleVote} />
+    <PollList on:vote={handleVote} />
   {:else if activeItem === 'Add New Poll'}
     <CreatePollForm on:add={handleAdd} />
   {/if}
