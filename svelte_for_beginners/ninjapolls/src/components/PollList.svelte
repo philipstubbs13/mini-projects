dd1bef36547dbd21a1ddbae14d9b7f47d4c42345
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-06 11:10:57 -0600

    lifecycle hooks

--- svelte_for_beginners/ninjapolls/src/components/PollList.svelte
+++ svelte_for_beginners/ninjapolls/src/components/PollList.svelte
@@ -1,16 +1,28 @@
 <script>
+  // import { onMount, onDestroy } from 'svelte';
   import PollStore from '../stores/PollStore.js';
   import PollDetails from './PollDetails.svelte';
 
-  export let polls = [];
+  // export let polls = [];
 
-  PollStore.subscribe(data => {
-    polls = data;
-  });
+  // const unsub = PollStore.subscribe(data => {
+  //   polls = data;
+  // });
+
+  // onMount(() => {
+  //   // maybe get data from a db
+  //   console.log('component mounted');
+  // });
+
+  // onDestroy(() => {
+  //   // unsub from a store
+  //   console.log('component destroyed');
+  //   unsub();
+  // });
 </script>
 
 <div class="poll-list">
-  {#each polls as poll (poll.id)}
+  {#each $PollStore as poll (poll.id)}
     <div>
       <PollDetails {poll} on:vote />
     </div>
