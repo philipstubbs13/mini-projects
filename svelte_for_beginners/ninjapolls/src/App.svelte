d557381dc03a13beba6d2bbf51ca961f8489f460
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-06 04:10:41 -0600

    poll form component

--- svelte_for_beginners/ninjapolls/src/App.svelte
+++ svelte_for_beginners/ninjapolls/src/App.svelte
@@ -2,6 +2,7 @@
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
   import Tabs from './shared/Tabs.svelte';
+  import CreatePollForm from './components/CreatePollForm.svelte';
 
   // tabs
   let items = ['Current Polls', 'Add New Poll'];
@@ -17,7 +18,7 @@
   {#if activeItem === 'Current Polls'}
     <p>Poll list component goes here</p>
   {:else if activeItem === 'Add New Poll'}
-    <p>new poll form component goes here</p>
+    <CreatePollForm />
   {/if}
 </main>
 <Footer />
