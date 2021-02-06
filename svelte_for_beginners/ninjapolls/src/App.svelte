e4c969b6b79317135dba35d0f9c2eac7d64f8e79
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-06 04:55:01 -0600

    adding new poll

--- svelte_for_beginners/ninjapolls/src/App.svelte
+++ svelte_for_beginners/ninjapolls/src/App.svelte
@@ -3,6 +3,7 @@
   import Footer from './components/Footer.svelte';
   import Tabs from './shared/Tabs.svelte';
   import CreatePollForm from './components/CreatePollForm.svelte';
+  import PollList from './components/PollList.svelte';
 
   // tabs
   let items = ['Current Polls', 'Add New Poll'];
@@ -10,15 +11,33 @@
   const tabChange = (e) => {
     activeItem = e.detail;
   }
+
+  let polls = [
+    {
+      id: 1,
+      question: 'Python or JavaScript?',
+      answerA: 'Python',
+      answerB: 'JavaScript',
+      votesA: 9,
+      votesB: 15,
+    }
+  ];
+
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    console.log(polls);
+    activeItem = 'Current Polls';
+  }
 </script>
 
 <Header />
 <main>
   <Tabs {activeItem} {items} on:tabChange={tabChange} />
   {#if activeItem === 'Current Polls'}
-    <p>Poll list component goes here</p>
+    <PollList {polls} />
   {:else if activeItem === 'Add New Poll'}
-    <CreatePollForm />
+    <CreatePollForm on:add={handleAdd} />
   {/if}
 </main>
 <Footer />
--- svelte_for_beginners/ninjapolls/src/components/CreatePollForm.svelte
+++ svelte_for_beginners/ninjapolls/src/components/CreatePollForm.svelte
@@ -1,5 +1,8 @@
 <script>
   import Button from '../shared/button.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  let dispatch = createEventDispatcher();
   let fields = { question: '', answerA: '', answerB: '' };
   let errors = { question: '', answerA: '', answerB: '' };
   let valid =  false;
@@ -33,7 +36,8 @@
 
     // add new poll
     if (valid) {
-      console.log('valid', fields);
+      let poll = {...fields, votesA: 0, votesB: 0, id: Math.random()};
+      dispatch('add', poll);
     }
   }
 </script>
