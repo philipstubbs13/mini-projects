d1be6dc26c3d82f877f3f774085f6b8b1bfb83f3
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-06 04:01:32 -0600

    reusable tab components

--- svelte_for_beginners/ninjapolls/src/App.svelte
+++ svelte_for_beginners/ninjapolls/src/App.svelte
@@ -1,11 +1,24 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import Tabs from './shared/Tabs.svelte';
+
+  // tabs
+  let items = ['Current Polls', 'Add New Poll'];
+  let activeItem = 'Current Polls';
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  }
 </script>
 
 <Header />
 <main>
-
+  <Tabs {activeItem} {items} on:tabChange={tabChange} />
+  {#if activeItem === 'Current Polls'}
+    <p>Poll list component goes here</p>
+  {:else if activeItem === 'Add New Poll'}
+    <p>new poll form component goes here</p>
+  {/if}
 </main>
 <Footer />
 
