c82e53fa4098865506ae3f53541dbf0ddb151e0e
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-06 11:29:12 -0600

    deleting polls

--- svelte_for_beginners/ninjapolls/src/components/PollDetails.svelte
+++ svelte_for_beginners/ninjapolls/src/components/PollDetails.svelte
@@ -1,6 +1,7 @@
 <script>
   import Card from '../shared/Card.svelte';
   import PollStore from '../stores/PollStore.js';
+  import Button from '../shared/Button.svelte';
 
   export let poll;
 
@@ -25,6 +26,13 @@
       return copiedPolls;
     });
   }
+
+  // deleting a poll
+  const handleDelete = (id) => {
+    PollStore.update(currentPolls => {
+      return currentPolls.filter(poll => poll.id != id);
+    });
+  };
 </script>
 
 <Card>
@@ -38,6 +46,9 @@
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
       <div class="percent percent-b" style="width: {percentB}%"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
+    </div>
+    <div class="delete">
+      <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
     </div>
   </div>
 </Card>
@@ -78,5 +89,10 @@
   .percent-b {
     border-left: 4px solid #45c496;
     background: rgba(69, 196, 150, 0.2);
+  }
+
+  .delete {
+    margin-top: 30px;
+    text-align: center;
   }
 </style>
