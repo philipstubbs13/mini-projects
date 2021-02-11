41b59243d20a4bf2aca0dad85507db2857df18bd
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-11 04:51:41 -0600

    react context and hooks - introduction

---
+++ react-context-hooks-tutorial/contextapp/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Booklist from './components/BookList';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Booklist />
+    </div>
+  );
+}
+
+export default App;
