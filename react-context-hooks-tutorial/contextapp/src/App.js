1a0a7606e61964a4dfd11d75f94c4419ad38e301
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-12 05:41:36 -0600

    updating context data

--- react-context-hooks-tutorial/contextapp/src/App.js
+++ react-context-hooks-tutorial/contextapp/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Booklist from './components/BookList';
 import ThemeContextProvider from './contexts/themeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <ThemeContextProvider>
         <Navbar />
         <Booklist />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
