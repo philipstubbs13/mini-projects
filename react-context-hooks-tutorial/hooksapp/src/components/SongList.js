96bb27e8afb7d814d6d567f731ad79e9de8dd39d
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-13 05:08:45 -0600

    usestate with forms

--- react-context-hooks-tutorial/hooksapp/src/components/SongList.js
+++ react-context-hooks-tutorial/hooksapp/src/components/SongList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    {title: 'almost home', id: 1},
+    {title: 'memory gospel', id: 2},
+    {title: 'this wild darkness', id:3}
+  ]);
+
+  const addSong = (title) => {
+    setSongs([...songs, {title, id: uuid()}])
+  }
+
   return (
     <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map(song => {
+          return (<li key={song.id}>{song.title}</li>);
+        })}
       </ul>
+      <NewSongForm addSong={addSong} />
     </div>
   )
 }
