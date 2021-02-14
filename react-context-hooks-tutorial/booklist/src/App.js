8cc9707345a5dc0313848cd9ea36f763d4d08faf
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-14 05:34:04 -0600

    reading list project part 1

---
+++ react-context-hooks-tutorial/booklist/src/App.js
@@ -0,0 +1,14 @@
+import BookContextProvider from './contexts/BookContext';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
+    </div>
+  );
+}
+
+export default App;
