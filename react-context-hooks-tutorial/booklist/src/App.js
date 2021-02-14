2fcddae2777c2f5c7549ef168a338e6caf4c4e0a
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-14 05:48:05 -0600

    book reading list part 2

--- react-context-hooks-tutorial/booklist/src/App.js
+++ react-context-hooks-tutorial/booklist/src/App.js
@@ -1,11 +1,13 @@
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
