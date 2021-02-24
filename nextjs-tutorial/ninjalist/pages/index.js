c37515e62d5028cad3354e1f9783bb636f04013c
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-24 06:14:17 -0600

    adding other components

--- nextjs-tutorial/ninjalist/pages/index.js
+++ nextjs-tutorial/ninjalist/pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
+      <Footer />
     </div>
   )
 }
