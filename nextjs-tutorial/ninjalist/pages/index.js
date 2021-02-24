e7d9933cf8cd1a91069f2919486d787a333af860
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-24 06:22:47 -0600

    linking pages

--- nextjs-tutorial/ninjalist/pages/index.js
+++ nextjs-tutorial/ninjalist/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
