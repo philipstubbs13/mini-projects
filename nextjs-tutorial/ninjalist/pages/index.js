dd133144746b9f31097c90ad26378f43ebefb875
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-24 06:28:44 -0600

    creating a layout component

--- nextjs-tutorial/ninjalist/pages/index.js
+++ nextjs-tutorial/ninjalist/pages/index.js
@@ -7,14 +7,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
