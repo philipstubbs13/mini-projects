93e3ad6732cdcebb6c9b0487d38bb8d1e87de9c0
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-24 06:10:01 -0600

    pages and routes

--- nextjs-tutorial/ninjalist/pages/about.js
+++ nextjs-tutorial/ninjalist/pages/about.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+const About = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1>About</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a omnis pariatur, iusto obcaecati facilis maiores magnam nulla reprehenderit eveniet eius neque vitae laudantium corrupti blanditiis laborum? Nam, veniam modi.</p>
     </div>
-  )
+  );
 }
+ 
+export default About;
