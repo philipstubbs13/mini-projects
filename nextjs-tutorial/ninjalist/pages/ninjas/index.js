b808613feae19da621a7cc5a41f737fd01e0fea8
philipstubbs13 <philipstubbs13@gmail.com>
2021-02-25 06:08:32 -0600

    dynamic routes part 1

--- nextjs-tutorial/ninjalist/pages/ninjas/index.js
+++ nextjs-tutorial/ninjalist/pages/ninjas/index.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/Ninjas.module.css';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
 
@@ -17,11 +18,11 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
