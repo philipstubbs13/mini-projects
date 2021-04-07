585e2cc18fba6dfbce69d596290580e6dd5dcf52
philipstubbs13 <philipstubbs13@gmail.com>
2021-04-06 20:21:33 -0500

    buttons

--- material_ui_tutorial/material-ui-tut/src/pages/Create.js
+++ material_ui_tutorial/material-ui-tut/src/pages/Create.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         color="textSecondary"
@@ -12,6 +14,25 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log('you clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        disableElevation
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit">Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
