5351e62145cd50ab401cd48e47a5a4d6fd55901c
philipstubbs13 <philipstubbs13@gmail.com>
2021-04-07 02:59:45 -0500

    radio buttons

--- material_ui_tutorial/material-ui-tut/src/pages/Create.js
+++ material_ui_tutorial/material-ui-tut/src/pages/Create.js
@@ -3,8 +3,12 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, FormControlLabel } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
 
 const useStyles = makeStyles({
   field: {
@@ -20,6 +24,7 @@ export default function Create() {
   const [details, setDetails] = useState('')
   const [titleError, setTitleError] = useState(false)
   const [detailsError, setDetailsError] = useState(false)
+  const [category, setCategory] = useState('todos')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -35,7 +40,7 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(title, details)
+      console.log(title, details, category)
     }
   }
 
@@ -73,6 +78,18 @@ export default function Create() {
           required
           error={detailsError}
         />
+
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+
+
         <Button
           type="submit"
           color="secondary"
