b75f9e7a3efda68f56a0c78a4dc64ccdf73aa58b
philipstubbs13 <philipstubbs13@gmail.com>
2021-04-06 20:29:11 -0500

    icons

--- material_ui_tutorial/material-ui-tut/src/pages/Create.js
+++ material_ui_tutorial/material-ui-tut/src/pages/Create.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 
 export default function Create() {
   return (
@@ -21,18 +22,10 @@ export default function Create() {
         color="secondary"
         variant="contained"
         disableElevation
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
-
-      {/* <Button type="submit">Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
-
-      <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
     </Container>
   )
 }
