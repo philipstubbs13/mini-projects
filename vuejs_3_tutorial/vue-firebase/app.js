72f6b6b7ee1e73f55b19b6dc12a77564738f15ec
philipstubbs13 <philipstubbs13@gmail.com>
2021-05-21 04:05:07 -0500

    vue basics part 2

--- vuejs_3_tutorial/vue-firebase/app.js
+++ vuejs_3_tutorial/vue-firebase/app.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'http://www.thenetninja.co.uk',
       showBooks: true,
       title: 'The Final Empire',
       author: 'Brandon Sanderson',
-      age: 45
+      age: 45,
+      x: 0,
+      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg'}
+      ]
     }
   },
   methods: {
@@ -13,7 +21,17 @@ const app = Vue.createApp({
       this.title = title
     },
     toggleShowBooks() {
-      this.showBooks = !this.showBooks
+      this.s,howBooks = !this.showBooks
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type)
+      if (data) {
+        console.log(data)
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
   }
 })
