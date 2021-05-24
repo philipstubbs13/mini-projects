f3ef2707fa143aae98aab1da30fc0631286a9659
philipstubbs13 <philipstubbs13@gmail.com>
2021-05-23 23:55:15 -0500

    vue basics part 2

--- vuejs_3_tutorial/vue-firebase/app.js
+++ vuejs_3_tutorial/vue-firebase/app.js
@@ -9,9 +9,9 @@ const app = Vue.createApp({
       x: 0,
       y: 0,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg'}
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true}
       ]
     }
   },
@@ -32,6 +32,14 @@ const app = Vue.createApp({
     handleMousemove(e) {
       this.x = e.offsetX
       this.y = e.offsetY
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav)
     }
   }
 })
