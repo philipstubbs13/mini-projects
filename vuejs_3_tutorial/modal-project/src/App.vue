0958a47cd97ac1d48b61dd650374806be3d8181f
philipstubbs13 <philipstubbs13@gmail.com>
2021-05-29 13:33:57 -0500

    props

--- vuejs_3_tutorial/modal-project/src/App.vue
+++ vuejs_3_tutorial/modal-project/src/App.vue
@@ -2,7 +2,7 @@
   <h1>{{ title }}</h1>
   <input type="text" ref="name">
   <button @click="handleClick">click me</button>
-  <Modal />
+  <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
@@ -15,7 +15,9 @@ export default {
   },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!'
     }
   },
   methods: {
