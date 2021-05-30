33076ee67ff88be13a04afe2489f370f04fdeaf9
philipstubbs13 <philipstubbs13@gmail.com>
2021-05-29 23:31:19 -0500

    creating another modal

--- vuejs_3_tutorial/modal-project/src/App.vue
+++ vuejs_3_tutorial/modal-project/src/App.vue
@@ -13,7 +13,15 @@
       <p>Grab your ninja swag for half price!</p>
     </Modal>
   </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </div>
   <button @click.alt="toggleModal">open modal (alt)</button>
+   <button @click="toggleModalTwo">open modal</button>
 </template>
 
 <script>
@@ -27,9 +35,8 @@ export default {
   data() {
     return {
       title: 'My First Vue App :)',
-      header: 'Sign up for the Giveaway!',
-      text: 'Grab your ninja swag for half price!',
-      showModal: false
+      showModal: false,
+      showModalTwo: false,
     }
   },
   methods: {
@@ -40,6 +47,9 @@ export default {
     },
     toggleModal() {
       this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
