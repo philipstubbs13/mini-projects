bbfc8adaad244844bf0478db3b1bde9391f4fdc1
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-02 23:01:20 -0500

    select boxes

--- vuejs_3_tutorial/web-form/src/components/SignupForm.vue
+++ vuejs_3_tutorial/web-form/src/components/SignupForm.vue
@@ -5,9 +5,16 @@
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -15,7 +22,8 @@ export default {
   data() {
     return {
       email: 'mario',
-      password: ''
+      password: '',
+      role: 'designer'
     }
   }
 }
@@ -39,7 +47,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
