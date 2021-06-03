8f503cd1ecab3df95c533c45553757d6a154a1a4
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-02 23:11:47 -0500

    checkboxes

--- vuejs_3_tutorial/web-form/src/components/SignupForm.vue
+++ vuejs_3_tutorial/web-form/src/components/SignupForm.vue
@@ -11,10 +11,30 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="shaun" v-model="names">
+      <label>Shaun</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yoshi" v-model="names">
+      <label>Yoshi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="mario" v-model="names">
+      <label>Mario</label>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+  <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -23,7 +43,9 @@ export default {
     return {
       email: 'mario',
       password: '',
-      role: 'designer'
+      role: 'designer',
+      terms: false,
+      names: []
     }
   }
 }
@@ -55,5 +77,12 @@ export default {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
   }
 </style>
