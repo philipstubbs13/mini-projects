8a0b5c6af7d8b03a4e68b9862be276ea5064e002
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-03 20:44:53 -0500

    submitting form

--- vuejs_3_tutorial/web-form/src/components/SignupForm.vue
+++ vuejs_3_tutorial/web-form/src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit:prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email">
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -22,6 +23,10 @@
       <input type="checkbox" v-model="terms" required>
       <label>Accept terms and conditions</label>
     </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -38,7 +43,8 @@ export default {
       role: 'designer',
       terms: false,
       tempSkill: '',
-      skills: []
+      skills: [],
+      passwordError: ''
     }
   },
   methods: {
@@ -54,6 +60,15 @@ export default {
       this.skills = this.skills.filter((item) => {
         return skill !== item
       })
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 chars long'
+
+      if (!this.passwordError) {
+        console.log('email', ths.email)
+        console.log('password', this.password)
+      }
     }
   }
 }
@@ -104,5 +119,22 @@ export default {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
   }
 </style>
