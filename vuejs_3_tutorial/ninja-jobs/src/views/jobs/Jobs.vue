d9630de35a9129eb893c09112d9fcd33b3440955
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-05 11:26:58 -0500

    dynamic links

--- vuejs_3_tutorial/ninja-jobs/src/views/jobs/Jobs.vue
+++ vuejs_3_tutorial/ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Jobs</h1>
   <div v-for="job in jobs" :key="job.id">
-    <h2>{{ job.title }}</h2>
+    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+      <h2>{{ job.title }}</h2>
+    </router-link>
   </div>
 </template>
 
