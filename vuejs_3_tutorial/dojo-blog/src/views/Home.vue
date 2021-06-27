c189d0da881f76ba12e0044208c7284bd8482940
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-27 12:09:26 -0500

    compisition api basics

---
+++ vuejs_3_tutorial/dojo-blog/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}.</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  setup() {
+    console.log('setup')
+
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    return { name, age, handleClick }
+  },
+}
+</script>
