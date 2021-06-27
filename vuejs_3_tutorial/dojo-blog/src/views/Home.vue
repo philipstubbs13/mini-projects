b9451d8e110f05d2fbd25acc43fb6d6a96c68072
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-27 12:16:15 -0500

    template refs

--- vuejs_3_tutorial/dojo-blog/src/views/Home.vue
+++ vuejs_3_tutorial/dojo-blog/src/views/Home.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} and my age is {{ age }}.</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}.</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    const p = ref(null)
 
     let name = 'mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello, ninjas'
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   },
 }
 </script>
