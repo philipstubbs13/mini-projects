ddc5be24a82ce848d0b3f315fa000f076be90d33
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-12 21:12:40 -0500

    styled components crash course

---
+++ styled_components_crash_course/README.md
@@ -0,0 +1,3 @@
+# Styled Components Crash Course and Project
+
+* <https://www.youtube.com/watch?v=02zO0hZmwnw>
--- styled_components_crash_course/hubble/src/App.js
+++ styled_components_crash_course/hubble/src/App.js
@@ -2,13 +2,17 @@ import { ThemeProvider } from 'styled-components';
 import { Container } from './components/styles/Container.styled'
 import Header from './components/Header';
 import GlobalStyles from './components/styles/Global';
+import content from './content';
+import Card from './components/Card';
+import Footer from './components/Footer';
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: '768px'
 }
 
 function App() {
@@ -18,8 +22,11 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
