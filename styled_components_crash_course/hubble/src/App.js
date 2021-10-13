eee1243e1134b74fd78c77d2fb95c54977943b59
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-12 20:25:59 -0500

    styled components crash course.

---
+++ styled_components_crash_course/hubble/src/App.js
@@ -0,0 +1,28 @@
+import { ThemeProvider } from 'styled-components';
+import { Container } from './components/styles/Container.styled'
+import Header from './components/Header';
+import GlobalStyles from './components/styles/Global';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ styled_components_crash_course/hubble/src/components/Header.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from './styles/Header.styled'
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <h1>Hubble</h1>
+    </StyledHeader>
+  )
+}
