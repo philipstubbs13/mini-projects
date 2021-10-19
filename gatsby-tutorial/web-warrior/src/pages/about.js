fb72d4dfad773eb898e69088bd779f73a0010234
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-19 05:43:36 -0500

    gatsby tutorial - pages and routes

---
+++ gatsby-tutorial/web-warrior/src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+        consequatur doloribus beatae maxime voluptatem temporibus similique
+        labore nam non quis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+        consequatur doloribus beatae maxime voluptatem temporibus similique
+        labore nam non quis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+        consequatur doloribus beatae maxime voluptatem temporibus similique
+        labore nam non quis!
+      </p>
+    </div>
+  )
+}
