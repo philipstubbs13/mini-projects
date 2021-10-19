189a0b367d20e9380d779f09ecbeda240f904d84
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-19 06:28:33 -0500

    static files

--- gatsby-tutorial/web-warrior/src/pages/index.js
+++ gatsby-tutorial/web-warrior/src/pages/index.js
@@ -1,15 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer and web developer based in Manchester.</p>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
+        <img
+          src="./banner.png"
+          alt="site banner"
+          style={{ maxWidth: "100%" }}
+        />
       </section>
     </Layout>
   )
--- gatsby-tutorial/web-warrior/src/pages/projects/index.js
+++ gatsby-tutorial/web-warrior/src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import { portfolio } from "../../styles/projects.module.css"
 
 export default function Project() {
   return (
     <Layout>
-      <div>
+      <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
       </div>
