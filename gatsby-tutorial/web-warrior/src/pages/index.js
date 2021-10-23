243098ea4a44888a28c267a60a290234d6de4a17
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-23 06:18:00 -0500

    optimized images

--- gatsby-tutorial/web-warrior/src/pages/index.js
+++ gatsby-tutorial/web-warrior/src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/Layout"
 import { header, btn } from "../styles/home.module.css"
+import Img from "gatsby-image"
 
 export default function Home({ data }) {
   return (
@@ -15,12 +16,20 @@ export default function Home({ data }) {
             My Portfolio Projects
           </Link>
         </div>
-        <img
-          src="./banner.png"
-          alt="site banner"
-          style={{ maxWidth: "100%" }}
-        />
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
