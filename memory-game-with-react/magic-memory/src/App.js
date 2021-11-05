6cda997365bd68dd2bd4d44203ad54226400fbd6
philipstubbs13 <philipstubbs13@gmail.com>
2021-11-05 06:22:49 -0500

    flipping cards

--- memory-game-with-react/magic-memory/src/App.js
+++ memory-game-with-react/magic-memory/src/App.js
@@ -49,7 +49,7 @@ function App() {
         resetTurn()
       } else {
         
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000)
       }
     }
   }, [choiceOne, choiceTwo])
@@ -72,6 +72,7 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
           />
         ))}
       </div>
