a84c3fb8c8b0a061abd9117cfff9b4570e984bc6
philipstubbs13 <philipstubbs13@gmail.com>
2021-12-19 13:03:44 -0600

    react chart js crash course

---
+++ react-chart-js-crash-course/README.md
@@ -0,0 +1,4 @@
+# React Chart JS Crash Course
+
+* <https://www.youtube.com/watch?v=Ge6PQkpa6pA>
+* <https://react-chartjs-2.netlify.app/>
---
+++ react-chart-js-crash-course/react-charts/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DoughnutChart from './charts/Doughnut';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ width: "500px", margin: "0 auto" }}>
+         <DoughnutChart />
+      </div>
+    </div>
+  );
+}
+
+export default App;
