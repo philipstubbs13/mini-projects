b7eb17e1995068f680d92f597a2fd01ef2ddc09c
philipstubbs13 <philipstubbs13@gmail.com>
2021-12-19 19:44:04 -0600

    remix crash course

---
+++ remix-js-crash-course-jokes-app/jokes-app/app/root.tsx
@@ -0,0 +1,41 @@
+import { LiveReload, Outlet, Links, LinksFunction } from "remix"
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)"
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)"
+    }
+  ];
+};
+
+export default function App() {
+
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix Jokes App</title>
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+      </body>
+    </html>
+  )
+}
---
+++ remix-js-crash-course-jokes-app/jokes-app/app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,11 @@
+export default function JokeRoute() {
+  return (
+    <div>
+      <p>Here's your joke:</p>
+      <p>
+        Why don't you find hippos hiding in tree?
+        They're really good at it.
+      </p>
+    </div>
+  )
+}
