bf92b981b04eb8ced897d018a2db7a81d29c76bc
philipstubbs13 <philipstubbs13@gmail.com>
2022-01-02 09:21:38 -0600

    mui 5 crash course

--- mui5-crash-course/my-mui-cc/src/pages/Tour.js
+++ mui5-crash-course/my-mui-cc/src/pages/Tour.js
@@ -1,8 +1,11 @@
-import { Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import ImageCollage from '../components/ImageCollage';
 import CustomizedAccordions from '../components/Accordian';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BasicModal from '../components/Modal';
 
 const Tour = () => (
   <Container sx={{ width: 900 }}>
@@ -21,12 +24,17 @@ const Tour = () => (
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam illum minus itaque corrupti, animi fuga earum minima dolores atque nihil at corporis. Odit repudiandae deleniti eum tempora corrupti facilis voluptas rem culpa sunt eos distinctio aut veritatis, laboriosam facere!
       </Typography>
     </Box>
-    <Box>
+    <Box marginBottom={10}>
       <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
         Frequently Asked Questions
       </Typography>
       <CustomizedAccordions />
     </Box>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation>
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
   </Container>
 )
 
