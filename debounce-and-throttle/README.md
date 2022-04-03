df18552468708c6fddafd54529eea07f3610904a
Phil <philipstubbs13@gmail.com>
2022-04-03 12:24:06 -0500

    debounce and throttle

---
+++ debounce-and-throttle/README.md
@@ -0,0 +1,3 @@
+# Debounce and Throttle
+
+* <https://www.youtube.com/watch?v=cjIswDCKgu0>
---
+++ debounce-and-throttle/script.js
@@ -0,0 +1,65 @@
+const input = document.querySelector("input")
+const defaultText = document.getElementById("default")
+const debounceText = document.getElementById("debounce")
+const throttleText = document.getElementById("throttle")
+
+const updateDebounceText = debounce((text) => {
+    // debounceText.textContent = text;
+    incrementCount(debounceText)
+})
+const updateThrottleText = debounce((text) => {
+    // throttleText.textContent = text;
+    incrementCount(throttleText)
+})
+// input.addEventListener("input", e => {
+//     defaultText.textContent = e.target.value
+//     updateDebounceText(e.target.value)
+//     updateThrottleText(e.target.value)
+// })
+
+function debounce(cb, delay = 1000) {
+    let timeout
+
+    return (...args) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            cb(...args)
+        }, delay)
+    }
+}
+
+function throttle(cb, delay = 1000) {
+    let shouldWait = false;
+    let waitingArgs
+    const timeoutFunc = () => {
+        if (waitingArgs == null) {
+            shouldWait = false
+        } else {
+            cb(...waitingArgs)
+            waitingArgs = null
+            setTimeout(timeoutFunc, delay)
+        }
+    }
+        
+    return (...args) => {
+        if (shouldWait) {
+            waitingArgs = args
+            return
+        }
+
+        cb(...args)
+        shouldWait = true
+
+        setTimeout(timeoutFunc, delay)
+    }
+}
+
+document.addEventListener("mousemove", e => {
+    incrementCount(defaultText)
+    updateDebounceText()
+    updateThrottleText()
+})
+
+function incrementCount(element) {
+    element.textContent = (parseInt(element.innerText) || 0) + 1
+}
