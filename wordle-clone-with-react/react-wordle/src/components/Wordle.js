c1e235474ecf53ce631404ee2a4a1038705904d8
Phil <philipstubbs13@gmail.com>
2022-05-14 15:09:14 -0500

    wordle clone with react

--- wordle-clone-with-react/react-wordle/src/components/Wordle.js
+++ wordle-clone-with-react/react-wordle/src/components/Wordle.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWordle(solution)
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
       window.addEventListener('keyup', handleKeyup)
 
       if (isCorrect) {
-        console.log('congrats, you win!')
+        setTimeout(() => setShowModal(true), 2000)
         window.removeEventListener('keyup', handleKeyup);
       }
 
       if (turn > 5) {
-        console.log('unlucky, out of guesses')
+        setTimeout(() => setShowModal(true), 2000)
         window.removeEventListener('keyup', handleKeyup)
       }
 
@@ -28,6 +30,7 @@ export default function Wordle({ solution }) {
         <div>current guess = {currentGuess}</div>
         <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
         <Keypad usedKeys={usedKeys} />
+        {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
     </div>
   )
 }
