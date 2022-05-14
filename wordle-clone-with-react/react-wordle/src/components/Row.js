2c7d8e5973236652fa542f87e6848128f7f0e5d8
Phil <philipstubbs13@gmail.com>
2022-05-14 13:43:01 -0500

    react clone

--- wordle-clone-with-react/react-wordle/src/components/Row.js
+++ wordle-clone-with-react/react-wordle/src/components/Row.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 
   if (guess) {
       return (
@@ -10,6 +10,21 @@ export default function Row({ guess }) {
               ))}
           </div>
       )
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split('')
+
+    return (
+        <div className="row current">
+            {letters.map((letter, i) => (
+                <div key={i} className="filled">{letter}</div>
+            ))}
+            {[...Array(5 - letters.length)].map((_, i) => (
+                <div key={i}></div>
+            ))}
+        </div>
+    )
   }
 
   return (
