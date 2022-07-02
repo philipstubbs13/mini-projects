1645d198dd866120d8a19699e88e72f96f89ee2d
Phil <philipstubbs13@gmail.com>
2022-07-02 13:48:01 -0500

    mern stack tutorial

--- mern-stack-tutorial/mern-stack/backend/server.js
+++ mern-stack-tutorial/mern-stack/backend/server.js
@@ -1,21 +1,29 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
 
 // express app
 const app = express();
+const workoutRoutes = require('./routes/workouts')
 
 // middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-// listen  for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen  for requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
