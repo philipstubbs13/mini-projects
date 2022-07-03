4361bdd93d16a4b612248417b7f83f0f14b57f93
Phil <philipstubbs13@gmail.com>
2022-07-03 09:17:31 -0500

    fetching data

--- mern-stack-tutorial/mern-stack/backend/routes/workouts.js
+++ mern-stack-tutorial/mern-stack/backend/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {
     createWorkout,
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -17,13 +19,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({ mssg: 'DELETE a workout' })
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({ mssg: 'UPDATE a workout' })
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
