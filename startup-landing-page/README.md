66887af2bb0148d22a99f5f95aa779362c0681e6
Phil <philipstubbs13@gmail.com>
2022-10-23 20:31:54 -0500

    create a startup landing page with next.js

---
+++ startup-landing-page/README.md
@@ -0,0 +1,3 @@
+# Startup Landing Page With Next.js, TypeScript, and NextUI
+
+* <https://www.youtube.com/watch?v=NZShdCjvwb8>
---
+++ startup-landing-page/landing-page/pages/index.tsx
@@ -0,0 +1,74 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Button, Container, Navbar, Text, Grid, Col } from '@nextui-org/react';
+import InfoCard from '../components/InfoCard';
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      {/* Navbar */}
+      <Navbar isCompact variant={"static"}>
+        <Navbar.Brand>
+          <Text b color="inherit">
+            LearnToCode
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="md">
+          <Navbar.Link href="#">Learning Platform</Navbar.Link>
+          <Navbar.Link href="#">Community</Navbar.Link>
+          <Navbar.Link href="#">Contact Us</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link href="#">Login</Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+      {/* Jumbotron */}
+      <Grid.Container justify="center" css={{ "height": "500px", "backgroundImage":  "url(https://littlevisuals.co/images/sunset.jpg)"}}>
+        <Grid xs={12} sm={6} alignItems="center">
+          <Col css={{ "width": "100%" }}>
+            <Text weight={"bold"} size={70} css={{ "textAlign": "center" }}>The Education Platform</Text>
+            <Text weight={"bold"} size={70} css={{ "textAlign": "center" }}>Of The Future</Text>
+            <Button size="md" shadow color="gradient" css={{ "width": "100%", "marginTop": "10px" }}>Join For Free</Button>
+          </Col>
+        </Grid>
+      </Grid.Container>
+
+      {/* 3 Displaying Product Cards */}
+      <Grid.Container gap={2}>
+        <Grid xs={12} sm={4}>
+          <InfoCard
+            label="Course"
+            title="Learn Next.js With Cooper Codes"
+            imageURL='https://littlevisuals.co/images/red_dawn.jpg'
+            studentCount='3,500'
+          />
+        </Grid>
+        <Grid xs={12} sm={4}>
+          <InfoCard
+            label="Course"
+            title="Learn Apollo Server With Cooper Codes"
+            imageURL='https://littlevisuals.co/images/sunset.jpg'
+            studentCount='1,000'
+          />
+        </Grid>
+        <Grid xs={12} sm={4}>
+          <InfoCard
+            label="Course"
+            title="Create A Startup With Cooper Codes"
+            imageURL='https://littlevisuals.co/images/tail.jpg'
+            studentCount='5,000'
+          />
+        </Grid>
+      </Grid.Container>
+    </Container>
+  )
+}
+
+export default Home
