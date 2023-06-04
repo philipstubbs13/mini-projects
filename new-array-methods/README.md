a2f68bda9ee15ff77b85f30b036f90ad8a8bae8e
Phil Stubbs <philipstubbs13@gmail.com>
2023-06-04 00:40:10 -0500

    new array methods

---
+++ new-array-methods/README.md
@@ -0,0 +1,3 @@
+# New Array Methods
+
+* <https://www.youtube.com/watch?v=3CBD5JZJJKw>
---
+++ new-array-methods/script.js
@@ -0,0 +1,17 @@
+const people = ["Sally", "Kyle", "John"]
+// const newPeople = people.with(2, "New")
+// const sortedPeople = people.toSorted()
+// const reversedPeople = people.toReversed()
+// const copy = people.toSpliced(0, 2, "New")
+
+// people.splice(0, 2, "New")
+// const sortedPeople = [...people].sort()
+// const peopleCopy = [...people]
+// peopleCopy[2] = "New"
+// console.log("copy", peopleCopy)
+
+// console.log("New", newPeople)
+// console.log("Sorted", sortedPeople)
+// console.log("Reversed", reversedPeople)
+// console.log("Coyp", copy)
+console.log("original", people)
